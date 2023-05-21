b049de4c4112d99da9b463adb8d55282b78337de
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-05-20 20:56:08 -0400

    Enhancement: eslint setup to support json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,37 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  env: {
+    jest: true,
+    node: true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsonc/recommended-with-jsonc',
     'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  ignorePatterns: ['package-lock.json'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    extraFileExtensions: ['.json'],
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  root: true,
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'jsonc/sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
   },
 };
