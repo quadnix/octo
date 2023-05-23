4a3dda64cd6efbf47b1d2f130c52f05b46b14f59
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-05-22 21:06:58 -0400

    Enhancement: restrict cross imports from another version.
    This is not an optimized rule as someone has to remember to update it when new versions come online.
    Ideally we would have liked to have a rule to say restrict all imports that come outside of that directory.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,13 +19,32 @@ module.exports = {
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'spellcheck'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import', 'spellcheck'],
   root: true,
   rules: {
     '@typescript-eslint/explicit-function-return-type': 2,
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          {
+            from: ['./src/v1', './src/v2'],
+            target: './src/v0',
+          },
+          {
+            from: ['./src/v0', './src/v2'],
+            target: './src/v1',
+          },
+          {
+            from: ['./src/v0', './src/v1'],
+            target: './src/v2',
+          },
+        ],
+      },
+    ],
     'jsonc/sort-keys': [
       'error',
       'asc',
@@ -43,5 +62,14 @@ module.exports = {
         skipWords: readFileSync('./dictionary.dic').toString().split('\n'),
       },
     ],
+  },
+  settings: {
+    // Fix to make `no-restricted-paths` rule work.
+    // Source: https://github.com/import-js/eslint-plugin-import/issues/1928#issuecomment-715164532
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
   },
 };
