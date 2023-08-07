1dbc64a115cb4bb1d65bca41e76300492d4a6a3d
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-08-06 20:30:06 -0400

    Enhancement: better tests for model E2E.

--- packages/octo/test/models/model.e2e-spec.ts
+++ packages/octo/test/models/model.e2e-spec.ts
@@ -13,10 +13,10 @@ import {
 } from '../../src';
 
 describe('Model E2E Test', () => {
-  describe('getContext() & synth()', () => {
+  describe('common functions', () => {
     const app = new App('test');
     const image = new Image('nginx', '0.0.1', {
-      dockerFilePath: __dirname + '/Dockerfile',
+      dockerFilePath: '/Dockerfile',
     });
     const pipeline = new Pipeline('testPipeline');
     const region = new Region('region-1');
@@ -73,11 +73,23 @@ describe('Model E2E Test', () => {
     ];
 
     describe.each(testCases)('Test $model.MODEL_NAME', (data) => {
-      it('should get context', () => {
+      it('getAncestors()', () => {
+        expect(data.model.getAncestors().map((m) => m.getContext())).toMatchSnapshot();
+      });
+
+      it('getChildren()', () => {
+        expect(Object.keys(data.model.getChildren())).toMatchSnapshot();
+      });
+
+      it('getContext()', () => {
         expect(data.model.getContext()).toMatchSnapshot();
       });
 
-      it('should be able to synth', () => {
+      it('getParents()', () => {
+        expect(Object.keys(data.model.getParents())).toMatchSnapshot();
+      });
+
+      it('synth()', () => {
         if (data.model.MODEL_NAME === 'service') {
           expect(() => {
             data.model.synth();
@@ -86,32 +98,6 @@ describe('Model E2E Test', () => {
           expect(data.model.synth()).toMatchSnapshot();
         }
       });
-    });
-  });
-
-  describe('getAncestors()', () => {
-    it('should get model ancestors', () => {
-      const app0 = new App('test-app');
-      const image0 = new Image('test', '0.0.1', { dockerFilePath: 'path' });
-      const region0 = new Region('region-0');
-      const region1 = new Region('region-1');
-      const server0 = new Server('backend');
-      const deployment0 = new Deployment('0.0.1', image0);
-      const environment0 = new Environment('qa');
-      const environment1 = new Environment('qa');
-      const execution0 = new Execution(deployment0, environment0);
-
-      app0.addImage(image0);
-      app0.addRegion(region0);
-      app0.addRegion(region1);
-      app0.addServer(server0);
-      server0.addDeployment(deployment0);
-      region0.addEnvironment(environment0);
-      region1.addEnvironment(environment1);
-
-      expect(app0.getAncestors().map((m) => m.getContext())).toMatchSnapshot();
-      expect(region0.getAncestors().map((m) => m.getContext())).toMatchSnapshot();
-      expect(execution0.getAncestors().map((m) => m.getContext())).toMatchSnapshot();
     });
   });
 });
