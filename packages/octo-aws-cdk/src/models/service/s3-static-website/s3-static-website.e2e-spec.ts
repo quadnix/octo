29437fa4e655698a48743870e1fc00d660415dfc
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-09-05 22:30:12 -0400

    Enhancement: S3 E2E test.

--- packages/octo-aws-cdk/src/models/service/s3-static-website/s3-static-website.e2e-spec.ts
+++ packages/octo-aws-cdk/src/models/service/s3-static-website/s3-static-website.e2e-spec.ts
@@ -1,100 +1,90 @@
-import { App, DiffService, LocalStateProvider, SerializationService, StateManagementService } from '@quadnix/octo';
+import { App, LocalStateProvider } from '@quadnix/octo';
 import axios from 'axios';
-import { unlink, writeFile } from 'fs';
+import { existsSync, unlink, writeFile } from 'fs';
 import { join } from 'path';
 import { promisify } from 'util';
-import { AWSRegionId, AwsRegion, OctoAws, S3StaticWebsiteService } from '../../../index';
+import { AwsRegion, AwsRegionId, OctoAws, S3StaticWebsiteService } from '../../../index';
 
 const writeFileAsync = promisify(writeFile);
 const unlinkAsync = promisify(unlink);
 
 const BUCKET_NAME = 'rash-new-bucket-test-1';
-const WEBSITE_PATH = join(__dirname, '../../../../resources/s3-static-website');
+
+const resourcesPath = join(__dirname, '../../../../resources');
+const websiteSourcePath = join(resourcesPath, 's3-static-website');
 
 describe('S3StaticWebsite E2E Test', () => {
-  const filePaths: string[] = [];
+  const filePaths: string[] = [
+    join(__dirname, 'aws-us-east-1a-models.json'),
+    join(__dirname, 'aws-us-east-1a-resources.json'),
+    join(__dirname, `${BUCKET_NAME}-manifest.json`),
+  ];
+
+  beforeEach(async () => {
+    const app = new App('test-app');
+    const region = new AwsRegion(AwsRegionId.AWS_US_EAST_1A);
+    app.addRegion(region);
+
+    const localStateProvider = new LocalStateProvider(__dirname);
+    const octoAws = new OctoAws(region, localStateProvider);
+    octoAws.registerInputs({
+      'input.region.aws-us-east-1a.subnet.private1.CidrBlock': '0.0.0.0/0',
+      'input.region.aws-us-east-1a.subnet.public1.CidrBlock': '0.0.0.0/0',
+      'input.region.aws-us-east-1a.vpc.CidrBlock': '0.0.0.0/0',
+    });
+
+    const diffs0 = await octoAws.diff();
+    const generator = await octoAws.beginTransaction(diffs0, {
+      yieldModelTransaction: true,
+      yieldResourceDiffs: true,
+    });
+
+    // Prevent generator from running real resource actions.
+    const modelTransactionResult = await generator.next();
+    const resourceDiffsResult = await generator.next();
+    await octoAws.commitTransaction(modelTransactionResult.value, resourceDiffsResult.value);
+  });
 
   afterEach(async () => {
-    await Promise.all(filePaths.map((f) => unlinkAsync(f)));
+    await Promise.all(filePaths.filter((f) => existsSync(f)).map((f) => unlinkAsync(f)));
   });
 
   it('should test working with a S3 bucket', async () => {
-    // Setup test cleanup.
-    filePaths.push(join(__dirname, 'infrastructure.json'));
-    filePaths.push(join(__dirname, `${BUCKET_NAME}-manifest.json`));
-
-    // Initialize local state.
-    const stateManagementService = StateManagementService.getInstance(new LocalStateProvider(__dirname));
-
-    // Initialize serializer.
-    const serializationService = new SerializationService();
-    serializationService.registerClass('S3StaticWebsiteService', S3StaticWebsiteService);
-
-    // Initialize Octo AWS.
-    const octoAws = new OctoAws(new AwsRegion(AWSRegionId.AWS_US_EAST_1));
-    const diffService = new DiffService();
-    diffService.registerActions(octoAws.getS3StaticWebsiteActions());
+    // Get previous state.
+    const localStateProvider = new LocalStateProvider(__dirname);
+    const previousApp = await OctoAws.getPreviousAppWithBoundary(AwsRegionId.AWS_US_EAST_1A, localStateProvider);
+    const previousRegion = previousApp!.getChild('region', [
+      { key: 'regionId', value: AwsRegionId.AWS_US_EAST_1A },
+    ]) as AwsRegion;
+    const octoAws = new OctoAws(previousRegion, localStateProvider);
 
     // Add website.
-    const app0 = new App('test-app');
-    const service0 = new S3StaticWebsiteService(BUCKET_NAME);
-    app0.addService(service0);
-    const diffs0 = await app0.diff();
-    const transaction0 = await diffService.beginTransaction(diffs0);
-    await stateManagementService.saveApplicationState(serializationService.serialize(app0));
-    /* eslint-disable spellcheck/spell-checker */
-    expect(transaction0).toMatchInlineSnapshot(`
-      [
-        [
-          {
-            "action": "add",
-            "field": "serviceId",
-            "value": "rash-new-bucket-test-1-s3-static-website",
-          },
-          {
-            "action": "update",
-            "field": "sourcePaths",
-            "value": {},
-          },
-        ],
-      ]
-    `);
-    /* eslint-enable */
+    const service = new S3StaticWebsiteService(previousRegion, BUCKET_NAME);
+    previousApp!.addService(service);
+
+    const diffs1 = await octoAws.diff();
+    let generator = await octoAws.beginTransaction(diffs1, {
+      yieldModelTransaction: true,
+      yieldResourceTransaction: true,
+    });
+
+    let modelTransactionResult = await generator.next();
+    let resourceTransactionResult = await generator.next();
+    await octoAws.commitTransaction(modelTransactionResult.value, resourceTransactionResult.value);
 
     // Add files to website.
-    const app0State = await stateManagementService.getApplicationState();
-    const app1 = (await serializationService.deserialize(app0State)) as App;
-    const service1 = app1.getChild('service', [
-      { key: 'serviceId', value: service0.serviceId },
-    ]) as S3StaticWebsiteService;
-    await service1.addSource(WEBSITE_PATH);
-    const diffs1 = await app1.diff(app0);
-    const transaction1 = await diffService.beginTransaction(diffs1);
-    await stateManagementService.saveApplicationState(serializationService.serialize(app1));
-    /* eslint-disable spellcheck/spell-checker */
-    expect(transaction1).toMatchInlineSnapshot(`
-      [
-        [
-          {
-            "action": "update",
-            "field": "sourcePaths",
-            "value": {
-              "error.html": {
-                "algorithm": "sha1",
-                "digest": "747c324737a310ff1c0ff1d3ab90d15cb00b585b",
-                "filePath": "${WEBSITE_PATH}/error.html",
-              },
-              "index.html": {
-                "algorithm": "sha1",
-                "digest": "aba92cd2086d7ab2f36d3bf5baa269478b941921",
-                "filePath": "${WEBSITE_PATH}/index.html",
-              },
-            },
-          },
-        ],
-      ]
-    `);
-    /* eslint-enable */
+    await service.addSource(`${websiteSourcePath}/error.html`);
+    await service.addSource(`${websiteSourcePath}/index.html`);
+
+    const diffs2 = await octoAws.diff();
+    generator = await octoAws.beginTransaction(diffs2, {
+      yieldModelTransaction: true,
+      yieldResourceTransaction: true,
+    });
+
+    modelTransactionResult = await generator.next();
+    resourceTransactionResult = await generator.next();
+    await octoAws.commitTransaction(modelTransactionResult.value, resourceTransactionResult.value);
 
     // Ensure website is available.
     const indexContent = await axios.get(`http://${BUCKET_NAME}.s3-website-us-east-1.amazonaws.com/index.html`);
@@ -103,62 +93,41 @@ describe('S3StaticWebsite E2E Test', () => {
     expect(errorContent.data).toContain('This is an error!');
 
     // Ensure page edits are captured and uploaded.
-    const app1State = await stateManagementService.getApplicationState();
-    const app2 = (await serializationService.deserialize(app1State)) as App;
     try {
-      await writeFileAsync(join(WEBSITE_PATH, 'error.html'), 'New error content!');
-      const diffs2 = await app2.diff(app1);
+      await writeFileAsync(join(websiteSourcePath, 'error.html'), 'New error content!');
+      const diffs3 = await octoAws.diff();
 
-      const transaction2 = await diffService.beginTransaction(diffs2);
-      await stateManagementService.saveApplicationState(serializationService.serialize(app2));
+      generator = await octoAws.beginTransaction(diffs3, {
+        yieldModelTransaction: true,
+        yieldResourceTransaction: true,
+      });
 
-      /* eslint-disable spellcheck/spell-checker */
-      expect(transaction2).toMatchInlineSnapshot(`
-        [
-          [
-            {
-              "action": "update",
-              "field": "sourcePaths",
-              "value": {
-                "error.html": {
-                  "algorithm": "sha1",
-                  "digest": "a975cdbac77bbb3c9201aa588218c4ca64604b4e",
-                  "filePath": "${WEBSITE_PATH}/error.html",
-                },
-                "index.html": {
-                  "algorithm": "sha1",
-                  "digest": "aba92cd2086d7ab2f36d3bf5baa269478b941921",
-                  "filePath": "${WEBSITE_PATH}/index.html",
-                },
-              },
-            },
-          ],
-        ]
-      `);
-      /* eslint-enable */
+      modelTransactionResult = await generator.next();
+      resourceTransactionResult = await generator.next();
+      await octoAws.commitTransaction(modelTransactionResult.value, resourceTransactionResult.value);
 
       const newErrorContent = await axios.get(`http://${BUCKET_NAME}.s3-website-us-east-1.amazonaws.com/error.html`);
       expect(newErrorContent.data).toContain('New error content!');
     } finally {
       // Restore error.html
-      await writeFileAsync(join(WEBSITE_PATH, 'error.html'), errorContent.data);
+      await writeFileAsync(join(websiteSourcePath, 'error.html'), errorContent.data);
     }
 
     // Remove website.
-    const app3 = new App('test-app');
-    const diffs3 = await app3.diff(app2);
-    const transaction3 = await diffService.beginTransaction(diffs3);
-    await stateManagementService.saveApplicationState(serializationService.serialize(app3));
-    expect(transaction3).toMatchInlineSnapshot(`
-      [
-        [
-          {
-            "action": "delete",
-            "field": "serviceId",
-            "value": "rash-new-bucket-test-1-s3-static-website",
-          },
-        ],
-      ]
-    `);
+    // const app3 = new App('test-app');
+    // const diffs3 = await app3.diff(app2);
+    // const transaction3 = await diffService.beginTransaction(diffs3);
+    // await stateManagementService.saveApplicationState(serializationService.serialize(app3));
+    // expect(transaction3).toMatchInlineSnapshot(`
+    //   [
+    //     [
+    //       {
+    //         "action": "delete",
+    //         "field": "serviceId",
+    //         "value": "rash-new-bucket-test-1-s3-static-website",
+    //       },
+    //     ],
+    //   ]
+    // `);
   });
 });
