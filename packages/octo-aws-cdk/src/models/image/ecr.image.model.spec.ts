786ac80fcb50cd61c2dd464d1a625ed7ac2ea3e5
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-09-16 21:23:28 -0400

    Enhancement: OctoAws support for Shared ECR Image.

--- packages/octo-aws-cdk/src/models/image/ecr.image.model.spec.ts
+++ packages/octo-aws-cdk/src/models/image/ecr.image.model.spec.ts
@@ -1,14 +1,17 @@
-import { App, Image, LocalStateProvider } from '@quadnix/octo';
+import { App, DiffMetadata, Image, LocalStateProvider, Resource } from '@quadnix/octo';
 import { existsSync, unlink } from 'fs';
 import { join } from 'path';
 import { promisify } from 'util';
 import { AwsRegionId, OctoAws } from '../../index';
+import { EcrImage } from '../../resources/ecr/ecr-image.resource';
 import { AwsRegion } from '../region/aws.region.model';
 
 const unlinkAsync = promisify(unlink);
 
 describe('ECRImage UT', () => {
   const filePaths: string[] = [
+    join(__dirname, 'aws-ap-south-1a-models.json'),
+    join(__dirname, 'aws-ap-south-1a-resources.json'),
     join(__dirname, 'aws-us-east-1a-models.json'),
     join(__dirname, 'aws-us-east-1a-resources.json'),
     join(__dirname, 'shared-resources.json'),
@@ -20,33 +23,55 @@ describe('ECRImage UT', () => {
 
   describe('diff()', () => {
     let app: App;
-    let region: AwsRegion;
+    let region1: AwsRegion;
+    let region2: AwsRegion;
 
-    let octoAws: OctoAws;
+    let octoAws1: OctoAws;
+    let octoAws2: OctoAws;
 
     beforeEach(async () => {
       app = new App('test');
-      region = new AwsRegion(AwsRegionId.AWS_US_EAST_1A);
-      app.addRegion(region);
+      region1 = new AwsRegion(AwsRegionId.AWS_US_EAST_1A);
+      app.addRegion(region1);
+      region2 = new AwsRegion(AwsRegionId.AWS_AP_SOUTH_1A);
+      app.addRegion(region2);
 
       const localStateProvider = new LocalStateProvider(__dirname);
-      octoAws = new OctoAws(region, localStateProvider);
-      octoAws.registerInputs({
+      octoAws1 = new OctoAws(region1, localStateProvider);
+      octoAws1.registerInputs({
         'input.region.aws-us-east-1a.subnet.private1.CidrBlock': '0.0.0.0/0',
         'input.region.aws-us-east-1a.subnet.public1.CidrBlock': '0.0.0.0/0',
         'input.region.aws-us-east-1a.vpc.CidrBlock': '0.0.0.0/0',
       });
 
-      const diffs0 = await octoAws.diff();
-      const generator = await octoAws.beginTransaction(diffs0, {
+      const diffs1_0 = await octoAws1.diff();
+      const generator1 = await octoAws1.beginTransaction(diffs1_0, {
         yieldModelTransaction: true,
-        yieldResourceDiffs: true,
+        yieldNewResources: true,
       });
 
-      // Prevent generator from running real resource actions.
-      const modelTransactionResult = await generator.next();
-      const resourceDiffsResult = await generator.next();
-      await octoAws.commitTransaction(modelTransactionResult.value, resourceDiffsResult.value);
+      // Prevent generator1 from running real resource actions.
+      const modelTransactionResult1 = (await generator1.next()) as IteratorResult<DiffMetadata[][]>;
+      const resourcesResult1 = (await generator1.next()) as IteratorResult<Resource<unknown>[]>;
+      await octoAws1.commitTransaction(modelTransactionResult1.value, resourcesResult1.value);
+
+      octoAws2 = new OctoAws(region2, localStateProvider);
+      octoAws2.registerInputs({
+        'input.region.aws-ap-south-1a.subnet.private1.CidrBlock': '0.0.0.0/0',
+        'input.region.aws-ap-south-1a.subnet.public1.CidrBlock': '0.0.0.0/0',
+        'input.region.aws-ap-south-1a.vpc.CidrBlock': '0.0.0.0/0',
+      });
+
+      const diffs2_0 = await octoAws2.diff();
+      const generator2 = await octoAws2.beginTransaction(diffs2_0, {
+        yieldModelTransaction: true,
+        yieldNewResources: true,
+      });
+
+      // Prevent generator2 from running real resource actions.
+      const modelTransactionResult2 = (await generator2.next()) as IteratorResult<DiffMetadata[][]>;
+      const resourcesResult2 = (await generator2.next()) as IteratorResult<Resource<unknown>[]>;
+      await octoAws2.commitTransaction(modelTransactionResult2.value, resourcesResult2.value);
     });
 
     it('should create new repository and image when none exist', async () => {
@@ -54,41 +79,67 @@ describe('ECRImage UT', () => {
         dockerFilePath: 'path/to/Dockerfile',
       });
       app.addImage(image);
-      image.addRelationship('imageId', region, 'regionId');
+      image.addRelationship('imageId', region1, 'regionId');
 
-      octoAws.registerInputs({
+      octoAws1.registerInputs({
         'input.image.quadnix/test:0.0.1.dockerExecutable': 'docker',
       });
 
-      const diffs1 = await octoAws.diff();
-      const generator = await octoAws.beginTransaction(diffs1, {
+      const diffs1_1 = await octoAws1.diff();
+      const generator1 = await octoAws1.beginTransaction(diffs1_1, {
         yieldModelTransaction: true,
+        yieldNewResources: true,
         yieldResourceDiffs: true,
       });
 
-      // Prevent generator from running real resource actions.
-      const modelTransactionResult = await generator.next();
-      const resourceDiffsResult = await generator.next();
+      // Prevent generator1 from running real resource actions.
+      const modelTransactionResult1 = (await generator1.next()) as IteratorResult<DiffMetadata[][]>;
+      const resourcesResult1 = (await generator1.next()) as IteratorResult<Resource<unknown>[]>;
+      const resourceDiffsResult1 = await generator1.next();
 
-      expect(modelTransactionResult.value).toMatchInlineSnapshot(`
-        [
-          [
-            {
-              "action": "add",
-              "field": "imageId",
-              "value": "quadnix/test:0.0.1",
-            },
-          ],
-        ]
-      `);
-
-      expect(resourceDiffsResult.value).toMatchInlineSnapshot(`
+      // Verify resource diff was as expected.
+      expect(resourceDiffsResult1.value).toMatchInlineSnapshot(`
         [
           [
             {
               "action": "add",
               "field": "resourceId",
               "value": "image-quadnix/test:0.0.1",
+            },
+          ],
+        ]
+      `);
+
+      // Fabricate resource, as if resource transaction was applied.
+      (resourceDiffsResult1.value[0][0].model as Resource<EcrImage>).response.sourceStringified = JSON.stringify({
+        awsRegion: region1.nativeAwsRegionId,
+      });
+      await octoAws1.commitTransaction(modelTransactionResult1.value, resourcesResult1.value);
+
+      // Add the same image to another region.
+      image.removeRelationship(region1);
+      image.addRelationship('imageId', region2, 'regionId');
+
+      octoAws2.registerInputs({
+        'input.image.quadnix/test:0.0.1.dockerExecutable': 'docker',
+      });
+
+      const diffs2_1 = await octoAws2.diff();
+      const generator2 = await octoAws2.beginTransaction(diffs2_1, {
+        yieldResourceDiffs: true,
+      });
+
+      // Prevent generator2 from running real resource actions.
+      const resourceDiffsResult2 = await generator2.next();
+
+      // Verify resource diff was as expected.
+      expect(resourceDiffsResult2.value).toMatchInlineSnapshot(`
+        [
+          [
+            {
+              "action": "update",
+              "field": "replicationsStringified",
+              "value": "ADD",
             },
           ],
         ]
--- packages/octo-aws-cdk/src/resources/ecr/ecr-image.shared-resource.ts
+++ packages/octo-aws-cdk/src/resources/ecr/ecr-image.shared-resource.ts
@@ -7,22 +7,32 @@ export class SharedEcrImage extends SharedResource<EcrImage> {
     super(resource);
   }
 
+  /**
+   * An ECR image can only be added, or removed.
+   * Presence of `previous` indicates if an ECR repository has already been created.
+   * If so, we must rely on ECR replication.
+   * If not, we can assume this is the only resource of its kind.
+   */
   override async diff(previous?: SharedEcrImage): Promise<Diff[]> {
     const diffs: Diff[] = [];
-    const ecrImageSource: IEcrImageMetadata =
-      (this.response?.sourceStringified as string)?.length > 0
-        ? JSON.parse(this.response.sourceStringified as string)
-        : {};
 
-    if (ecrImageSource.awsRegion) {
-      this.diffMarkers.update = { key: 'replicationsStringified', value: this.diffMarkers.delete ? 'DELETE' : 'ADD' };
-      diffs.push(new Diff(this, DiffAction.UPDATE, this.diffMarkers.update.key, this.diffMarkers.update.value));
-    } else {
-      if (!previous && this.diffMarkers.delete) {
-        diffs.push(new Diff(this, DiffAction.DELETE, 'resourceId', this.resourceId));
-      } else if (!previous) {
-        diffs.push(new Diff(this, DiffAction.ADD, 'resourceId', this.resourceId));
+    if (previous) {
+      const ecrImageSource: IEcrImageMetadata =
+        (previous.response?.sourceStringified as string)?.length > 0
+          ? JSON.parse(previous.response.sourceStringified as string)
+          : {};
+
+      if (ecrImageSource.awsRegion) {
+        this.diffMarkers.update = { key: 'replicationsStringified', value: this.diffMarkers.delete ? 'DELETE' : 'ADD' };
+        diffs.push(new Diff(this, DiffAction.UPDATE, this.diffMarkers.update.key, this.diffMarkers.update.value));
+        return diffs;
       }
+    }
+
+    if (this.diffMarkers.delete) {
+      diffs.push(new Diff(this, DiffAction.DELETE, 'resourceId', this.resourceId));
+    } else {
+      diffs.push(new Diff(this, DiffAction.ADD, 'resourceId', this.resourceId));
     }
 
     return diffs;
