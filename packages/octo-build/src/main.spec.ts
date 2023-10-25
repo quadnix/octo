2f5b5525a940c20e615e2b968afd178a29ce0d7c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-10-25 07:43:35 -0400

    Enhancement: OctoBuild UT for main.ts

--- packages/octo-build/src/main.spec.ts
+++ packages/octo-build/src/main.spec.ts
@@ -1,5 +1,11 @@
+import { jest } from '@jest/globals';
 import { resolve } from 'path';
-import { runCommand } from './main.js';
+import { runCommand, scheduleJobs } from './main.js';
+import { IBuildConfiguration } from './models/build-configuration.interface.js';
+import { IRunArguments } from './models/run-arguments.interface.js';
+import { StreamManager } from './streams/stream-manager.js';
+
+jest.mock('./streams/stream-manager.js');
 
 describe('Main Test', () => {
   describe('runCommand()', () => {
@@ -23,6 +29,46 @@ describe('Main Test', () => {
       });
 
       await promiseToFinishCommand;
+    });
+  });
+
+  describe('scheduleJobs()', () => {
+    let mockRegisterStream: jest.Mock;
+
+    beforeEach(() => {
+      mockRegisterStream = jest.spyOn(StreamManager.prototype, 'registerStream') as jest.Mock;
+      mockRegisterStream.mockImplementation(() => {
+        /* do nothing */
+      });
+    });
+
+    it('should be able to schedule a single job', async () => {
+      const args: IRunArguments = {
+        buildFilePath: '',
+        concurrency: 2,
+        dryRun: false,
+        sourcePath: '.',
+        timeout: 1000,
+      };
+      const configuration: IBuildConfiguration = {
+        dist: '',
+        env: {},
+        jobs: {
+          job1: {
+            command: 'echo 1',
+            dependsOn: [],
+            onError: 'throw',
+            retry: 0,
+            timeout: 0,
+          },
+        },
+      };
+
+      const promises = scheduleJobs(args, configuration);
+      await Promise.all(promises);
+
+      expect(mockRegisterStream).toHaveBeenCalledTimes(1);
+      expect(mockRegisterStream.mock.calls[0][0]).toBe('job1');
     });
   });
 });
