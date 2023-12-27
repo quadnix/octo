fead59e661d8ce76d6e4debf4d2db415e0c68638
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-12-27 05:52:58 -0500

    feat(octo-contribution-docs): initial commit.

---
+++ apps/octo-contribution-docs/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['../../.eslintrc.cjs'],
+  ignorePatterns: ['.docusaurus', 'build', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: '2022',
+    extraFileExtensions: ['.json'],
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  rules: {
+    '@nx/enforce-module-boundaries': [
+      'error',
+      {
+        allow: [],
+        depConstraints: [
+          {
+            onlyDependOnLibsWithTags: ['scope:octo-contribution-docs'],
+            sourceTag: 'scope:octo-contribution-docs',
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ apps/octo-contribution-docs/README.md
@@ -0,0 +1,3 @@
+# Octo-Contribution-Docs
+Octo-Contribution-Docs is the one-stop static documentation website for everything you need to contribute in Octo.
+This website is built using [Docusaurus 3](https://docusaurus.io/), a modern static website generator.
