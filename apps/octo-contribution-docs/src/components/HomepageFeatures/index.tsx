0d88c94cc9af6005ba665f264c9841826b83e250
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2023-12-29 05:36:12 -0500

    feat(*): eslint cleanup.

--- apps/octo-contribution-docs/src/components/HomepageFeatures/index.tsx
+++ apps/octo-contribution-docs/src/components/HomepageFeatures/index.tsx
@@ -10,38 +10,41 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
+        quickly.
+      </>
+    ),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'Easy to Use',
   },
   {
-    title: 'Focus on What Matters',
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
+        <code>docs</code> directory.
+      </>
+    ),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: 'Focus on What Matters',
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
+        header and footer.
       </>
     ),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Powered by React',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ description, Svg, title }: FeatureItem): JSX.Element {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
