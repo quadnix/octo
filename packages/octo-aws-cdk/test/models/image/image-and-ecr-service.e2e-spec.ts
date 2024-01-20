4b8d4efef464c65d31ff6bcba1aad3badb7f7cc6
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-01-20 07:13:44 -0500

    feat(octo-aws-cdk): image and ecr e2e test.

---
+++ packages/octo-aws-cdk/test/models/image/image-and-ecr-service.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { App, DiffMetadata, Image, LocalStateProvider, UnknownResource } from '@quadnix/octo';
+import { existsSync, unlink } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+import { AwsRegionId, EcrService, OctoAws } from '../../../src/index.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const unlinkAsync = promisify(unlink);
+
+describe('Image & ECRService E2E Test', () => {
+  const filePaths: string[] = [
+    join(__dirname, 'models.json'),
+    join(__dirname, 'resources.json'),
+    join(__dirname, 'shared-resources.json'),
+  ];
+
+  afterEach(async () => {
+    await Promise.all(filePaths.filter((f) => existsSync(f)).map((f) => unlinkAsync(f)));
+  });
+
+  it('should test working with an Image and ECR', async () => {
+    const octoAws = new OctoAws();
+    await octoAws.initialize(new LocalStateProvider(__dirname));
+    octoAws.registerInputs({
+      'input.image.quadnix/test:0.0.1.dockerExecutable': 'docker',
+    });
+
+    const app = new App('test');
+    const image1 = new Image('quadnix/test', '0.0.1', {
+      dockerfilePath: join(__dirname, '../../../../../resources/images/quadnix/nginx/0.0.1/Dockerfile'),
+    });
+    app.addImage(image1);
+    const service = new EcrService('quadnix/test');
+    service.addRegion(AwsRegionId.AWS_US_EAST_1A);
+    service.addImage(image1);
+    app.addService(service);
+
+    const diffs1 = await octoAws.diff(app);
+    const generator1 = await octoAws.beginTransaction(diffs1, {
+      yieldModelTransaction: true,
+      yieldNewResources: true,
+    });
+
+    const modelTransactionResult1 = (await generator1.next()) as IteratorResult<DiffMetadata[][]>;
+    const resourcesResult1 = (await generator1.next()) as IteratorResult<UnknownResource[]>;
+    await generator1.next(); // Run real resource actions.
+    await octoAws.commitTransaction(app, modelTransactionResult1.value, resourcesResult1.value);
+
+    // Remove image.
+    service.removeImage('quadnix/test', '0.0.1');
+
+    const diffs2 = await octoAws.diff(app);
+    const generator2 = await octoAws.beginTransaction(diffs2, {
+      yieldModelTransaction: true,
+      yieldNewResources: true,
+    });
+
+    const modelTransactionResult2 = (await generator2.next()) as IteratorResult<DiffMetadata[][]>;
+    const resourcesResult2 = (await generator2.next()) as IteratorResult<UnknownResource[]>;
+    await generator2.next(); // Run real resource actions.
+    await octoAws.commitTransaction(app, modelTransactionResult2.value, resourcesResult2.value);
+  }, 60_000);
+});
