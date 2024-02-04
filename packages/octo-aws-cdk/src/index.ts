487e26fa56ad3dadd32f45139fc8fe5761415656
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-02-04 06:34:20 -0500

    feat: octo-aws-cdk index update.

--- packages/octo-aws-cdk/src/index.ts
+++ packages/octo-aws-cdk/src/index.ts
@@ -9,72 +9,76 @@ import './factories/aws/iam.aws.factory.js';
 import './factories/aws/s3.aws.factory.js';
 import './factories/aws/sts.aws.factory.js';
 
-import './models/environment/actions/add-environment.action.js';
-import './models/environment/actions/delete-environment.action.js';
+import './models/environment/actions/add-environment.model.action.js';
+import './models/environment/actions/delete-environment.model.action.js';
 
-export { EcrImage } from './models/image/ecr.image.model.js';
-import './models/image/actions/add-image.action.js';
-import './models/image/actions/delete-image.action.js';
+import './models/image/actions/add-image.model.action.js';
+import './models/image/actions/delete-image.model.action.js';
 
-export { AwsRegion, AwsRegionId } from './models/region/aws.region.model.js';
-import './models/region/actions/add-region.action.js';
-import './models/region/actions/delete-region.action.js';
+export { AwsRegion, RegionId } from './models/region/aws.region.model.js';
+import './models/region/actions/add-region.model.action.js';
+import './models/region/actions/delete-region.model.action.js';
 
 export { AwsServer } from './models/server/aws.server.model.js';
-import './models/server/actions/add-server.action.js';
-import './models/server/actions/delete-server.action.js';
+import './models/server/actions/add-server.model.action.js';
+import './models/server/actions/delete-server.model.action.js';
+
+export { EcrService } from './models/service/ecr/ecr.service.model.js';
+import './models/service/ecr/actions/add-ecr-service.model.action.js';
+import './models/service/ecr/actions/add-image-to-ecr.model.action.js';
+import './models/service/ecr/actions/delete-image-from-ecr.model.action.js';
 
 export { S3StaticWebsiteService } from './models/service/s3-static-website/s3-static-website.service.model.js';
-import './models/service/s3-static-website/actions/add-s3-static-website.action.js';
-import './models/service/s3-static-website/actions/delete-s3-static-website.action.js';
-import './models/service/s3-static-website/actions/update-source-paths-s3-static-website.action.js';
+import './models/service/s3-static-website/actions/add-s3-static-website.model.action.js';
+import './models/service/s3-static-website/actions/delete-s3-static-website.model.action.js';
+import './models/service/s3-static-website/actions/update-source-paths-s3-static-website.model.action.js';
 
 export { S3StorageService } from './models/service/s3-storage/s3-storage.service.model.js';
-import './models/service/s3-storage/actions/add-s3-storage.action.js';
-import './models/service/s3-storage/actions/delete-s3-storage.action.js';
-import './models/service/s3-storage/actions/update-directories-s3-storage.action.js';
+import './models/service/s3-storage/actions/add-s3-storage.model.action.js';
+import './models/service/s3-storage/actions/delete-s3-storage.model.action.js';
+import './models/service/s3-storage/actions/update-directories-s3-storage.model.action.js';
 
 export { AAction } from './models/action.abstract.js';
 
 export { NginxRouterModule } from './modules/routers/nginx.router.module.js';
 
-import './resources/ecr/actions/add-ecr-image.action.js';
-import './resources/ecr/actions/delete-ecr-image.action.js';
+import './resources/ecr/actions/add-ecr-image.resource.action.js';
+import './resources/ecr/actions/delete-ecr-image.resource.action.js';
 
-import './resources/ecs/actions/add-ecs-cluster.action.js';
-import './resources/ecs/actions/delete-ecs-cluster.action.js';
+import './resources/ecs/actions/add-ecs-cluster.resource.action.js';
+import './resources/ecs/actions/delete-ecs-cluster.resource.action.js';
 
-import './resources/efs/actions/add-efs.action.js';
-import './resources/efs/actions/delete-efs-action.js';
+import './resources/efs/actions/add-efs.resource.action.js';
+import './resources/efs/actions/delete-efs.resource.action.js';
 
-import './resources/iam/actions/add-iam-user.action.js';
-import './resources/iam/actions/delete-iam-user.action.js';
+import './resources/iam/actions/add-iam-user.resource.action.js';
+import './resources/iam/actions/delete-iam-user.resource.action.js';
 
-import './resources/internet-gateway/actions/add-internet-gateway.action.js';
-import './resources/internet-gateway/actions/delete-internet-gateway.action.js';
+import './resources/internet-gateway/actions/add-internet-gateway.resource.action.js';
+import './resources/internet-gateway/actions/delete-internet-gateway.resource.action.js';
 
-import './resources/network-acl/actions/add-network-acl.action.js';
-import './resources/network-acl/actions/delete-network-acl.action.js';
+import './resources/network-acl/actions/add-network-acl.resource.action.js';
+import './resources/network-acl/actions/delete-network-acl.resource.action.js';
 
-import './resources/route-table/actions/add-route-table.action.js';
-import './resources/route-table/actions/delete-route-table.action.js';
+import './resources/route-table/actions/add-route-table.resource.action.js';
+import './resources/route-table/actions/delete-route-table.resource.action.js';
 
-import './resources/s3/storage/actions/add-s3-storage.action.js';
-import './resources/s3/storage/actions/delete-s3-storage.action.js';
-import './resources/s3/storage/actions/update-add-directories-in-s3-storage.action.js';
-import './resources/s3/storage/actions/update-remove-directories-in-s3-storage.action.js';
+import './resources/s3/storage/actions/add-s3-storage.resource.action.js';
+import './resources/s3/storage/actions/delete-s3-storage.resource.action.js';
+import './resources/s3/storage/actions/update-add-directories-in-s3-storage.resource.action.js';
+import './resources/s3/storage/actions/update-remove-directories-in-s3-storage.resource.action.js';
 
-// import './resources/s3/website/actions/add-s3-website.action.js';
-// import './resources/s3/website/actions/delete-s3-website.action.js';
-// import './resources/s3/website/actions/update-source-paths-in-s3-website.action.js';
+import './resources/s3/website/actions/add-s3-website.resource.action.js';
+import './resources/s3/website/actions/delete-s3-website.resource.action.js';
+import './resources/s3/website/actions/update-source-paths-in-s3-website.resource.action.js';
 
-import './resources/security-groups/actions/add-security-group.action.js';
-import './resources/security-groups/actions/delete-security-group.action.js';
+import './resources/security-groups/actions/add-security-group.resource.action.js';
+import './resources/security-groups/actions/delete-security-group.resource.action.js';
 
-import './resources/subnet/actions/add-subnet.action.js';
-import './resources/subnet/actions/delete-subnet.action.js';
+import './resources/subnet/actions/add-subnet.resource.action.js';
+import './resources/subnet/actions/delete-subnet.resource.action.js';
 
-import './resources/vpc/actions/add-vpc.action.js';
-import './resources/vpc/actions/delete-vpc.action.js';
+import './resources/vpc/actions/add-vpc.resource.action.js';
+import './resources/vpc/actions/delete-vpc.resource.action.js';
 
 export { OctoAws } from './main.js';
