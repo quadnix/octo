644fd8ad672ac785b261f6ba663edb16b2000b1f
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-05-11 11:14:23 -0400

    patch(feat): octo

--- packages/octo/src/utilities/cidr/cidr.utility.spec.ts
+++ packages/octo/src/utilities/cidr/cidr.utility.spec.ts
@@ -1,21 +1,38 @@
 import { CidrUtility } from './cidr.utility.js';
 
 describe('CIDR Utility Test', () => {
-  describe('isCidrValidPrivateIpV4Cidr()', () => {
+  describe('addToIp()', () => {
+    it('should add capacity to given ip', () => {
+      expect(CidrUtility.addToIp('0.0.0.0', 1)).toBe('0.0.0.1');
+      expect(CidrUtility.addToIp('10.0.0.128', 127)).toBe('10.0.0.255');
+      expect(CidrUtility.addToIp('10.0.0.128', 128)).toBe('10.0.1.0');
+      expect(CidrUtility.addToIp('255.255.255.255', 1)).toBe('0.0.0.0');
+    });
+  });
+
+  describe('getCidrRange()', () => {
+    it('should return cidr range', () => {
+      expect(CidrUtility.getCidrRange('0.0.0.0', '0.0.0.0')).toEqual(['0.0.0.0/32']);
+      expect(CidrUtility.getCidrRange('10.0.0.0', '10.0.0.255')).toEqual(['10.0.0.0/24']);
+      expect(CidrUtility.getCidrRange('10.0.0.0', '10.0.1.255')).toEqual(['10.0.0.0/23']);
+    });
+  });
+
+  describe('isPrivateIpV4CidrRange()', () => {
     it('should return false when cidr is invalid', () => {
-      expect(CidrUtility.isValidPrivateIpV4Range('100')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('100.0.0.532/8')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('10.0.0.32')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('172.0.0.32')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('172.16.0.32/8')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('192.16.0.32/8')).toBe(false);
-      expect(CidrUtility.isValidPrivateIpV4Range('192.168.0.32/8')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('100')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('100.0.0.532/8')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('10.0.0.32')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('172.0.0.32')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('172.16.0.32/8')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('192.16.0.32/8')).toBe(false);
+      expect(CidrUtility.isPrivateIpV4CidrRange('192.168.0.32/8')).toBe(false);
     });
 
     it('should return true when cidr is valid', () => {
-      expect(CidrUtility.isValidPrivateIpV4Range('10.0.0.76/8')).toBe(true);
-      expect(CidrUtility.isValidPrivateIpV4Range('172.17.0.0/16')).toBe(true);
-      expect(CidrUtility.isValidPrivateIpV4Range('192.168.0.0/20')).toBe(true);
+      expect(CidrUtility.isPrivateIpV4CidrRange('10.0.0.76/8')).toBe(true);
+      expect(CidrUtility.isPrivateIpV4CidrRange('172.17.0.0/16')).toBe(true);
+      expect(CidrUtility.isPrivateIpV4CidrRange('192.168.0.0/20')).toBe(true);
     });
   });
 });
--- packages/octo/src/utilities/cidr/cidr.utility.ts
+++ packages/octo/src/utilities/cidr/cidr.utility.ts
@@ -1,5 +1,39 @@
 export class CidrUtility {
-  static isValidPrivateIpV4Range(cidrRange: string): boolean {
+  private static ipToNumber(ipAsString: string): number {
+    const octets = ipAsString.split('.').map((octet) => Number(octet));
+    return (octets[0] << 24) | (octets[1] << 16) | (octets[2] << 8) | (octets[3] << 0);
+  }
+
+  private static numberToIp(ipAsNumber: number): string {
+    return [
+      (ipAsNumber >> 24) & 0xff,
+      (ipAsNumber >> 16) & 0xff,
+      (ipAsNumber >> 8) & 0xff,
+      (ipAsNumber >> 0) & 0xff,
+    ].join('.');
+  }
+
+  static addToIp(startIp: string, capacity: number): string {
+    const startIpAsNumber = CidrUtility.ipToNumber(startIp);
+    return CidrUtility.numberToIp(startIpAsNumber + capacity);
+  }
+
+  static getCidrRange(startIp: string, endIp: string): string[] {
+    let startIpAsNumber = CidrUtility.ipToNumber(startIp);
+    const endIpAsNumber = CidrUtility.ipToNumber(endIp);
+
+    const cidrBlocks: string[] = [];
+    while (startIpAsNumber <= endIpAsNumber) {
+      const maxBits = 32 - Math.floor(Math.log2(endIpAsNumber - startIpAsNumber + 1));
+      const cidrBlock = CidrUtility.numberToIp(startIpAsNumber) + '/' + maxBits;
+      cidrBlocks.push(cidrBlock);
+
+      startIpAsNumber += Math.pow(2, 32 - maxBits);
+    }
+    return cidrBlocks;
+  }
+
+  static isPrivateIpV4CidrRange(cidrRange: string): boolean {
     const [firstIp, size = '32'] = cidrRange.split('/');
 
     const firstIpOctets = firstIp.split('.');
