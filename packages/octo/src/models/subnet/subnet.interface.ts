4a52dd3da40e10b006c55968de9112c8e958ad71
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-05-15 06:57:38 -0400

    patch(feat): octo subnet model with support for type and association.

--- packages/octo/src/models/subnet/subnet.interface.ts
+++ packages/octo/src/models/subnet/subnet.interface.ts
@@ -2,6 +2,7 @@ import { IModelReference } from '../model.interface.js';
 import { Subnet } from './subnet.model.js';
 
 export interface ISubnet {
+  options: Subnet['options'];
   region: IModelReference;
   subnetId: Subnet['subnetId'];
   subnetName: Subnet['subnetName'];
--- packages/octo/src/models/subnet/subnet.model.ts
+++ packages/octo/src/models/subnet/subnet.model.ts
@@ -1,12 +1,23 @@
 import { UnknownModel } from '../../app.type.js';
 import { Model } from '../../decorators/model.decorator.js';
+import { Diff, DiffAction } from '../../functions/diff/diff.js';
 import { AModel } from '../model.abstract.js';
 import { Region } from '../region/region.model.js';
 import { ISubnet } from './subnet.interface.js';
 
+enum SubnetType {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+}
+
 @Model()
 export class Subnet extends AModel<ISubnet, Subnet> {
   readonly MODEL_NAME: string = 'subnet';
+
+  private options: { disableSubnetIntraNetwork: boolean; subnetType: SubnetType } = {
+    disableSubnetIntraNetwork: false,
+    subnetType: SubnetType.PRIVATE,
+  };
 
   readonly subnetId: string;
 
@@ -17,6 +28,50 @@ export class Subnet extends AModel<ISubnet, Subnet> {
 
     this.subnetId = region.regionId + '-' + name;
     this.subnetName = name;
+  }
+
+  get disableSubnetIntraNetwork(): boolean {
+    return this.options.disableSubnetIntraNetwork;
+  }
+
+  set disableSubnetIntraNetwork(disableSubnetIntraNetwork: boolean) {
+    this.options.disableSubnetIntraNetwork = disableSubnetIntraNetwork;
+  }
+
+  get subnetType(): SubnetType {
+    return this.options.subnetType;
+  }
+
+  set subnetType(subnetType: SubnetType) {
+    this.options.subnetType = subnetType || SubnetType.PRIVATE;
+  }
+
+  override async diff(previous?: Subnet): Promise<Diff[]> {
+    const diffs: Diff[] = [];
+
+    // Generate diff of options.
+    if (previous && previous.disableSubnetIntraNetwork !== this.disableSubnetIntraNetwork) {
+      diffs.push(new Diff(this, DiffAction.UPDATE, 'disableSubnetIntraNetwork', this.disableSubnetIntraNetwork));
+    }
+    if (previous && previous.subnetType !== this.subnetType) {
+      throw new Error('Change of subnet type is not supported!');
+    }
+
+    // Generate diff of associations.
+    const children = this.getChildren()['subnet'];
+    const previousChildren = (previous?.getChildren() ?? { subnet: [] })['subnet'];
+    for (const pd of previousChildren) {
+      if (!children.find((d) => (d.to as Subnet).subnetId === (pd.to as Subnet).subnetId)) {
+        diffs.push(new Diff(this, DiffAction.DELETE, 'association', pd.to));
+      }
+    }
+    for (const d of children) {
+      if (!previousChildren.find((pd) => (pd.to as Subnet).subnetId === (d.to as Subnet).subnetId)) {
+        diffs.push(new Diff(this, DiffAction.ADD, 'association', d.to));
+      }
+    }
+
+    return diffs;
   }
 
   getContext(): string {
@@ -30,6 +85,7 @@ export class Subnet extends AModel<ISubnet, Subnet> {
     const region = parents['region'][0].to as Region;
 
     return {
+      options: { disableSubnetIntraNetwork: this.disableSubnetIntraNetwork, subnetType: this.subnetType },
       region: { context: region.getContext() },
       subnetId: this.subnetId,
       subnetName: this.subnetName,
@@ -41,6 +97,23 @@ export class Subnet extends AModel<ISubnet, Subnet> {
     deReferenceContext: (context: string) => Promise<UnknownModel>,
   ): Promise<Subnet> {
     const region = (await deReferenceContext(subnet.region.context)) as Region;
-    return new Subnet(region, subnet.subnetId);
+    const newSubnet = new Subnet(region, subnet.subnetId);
+
+    newSubnet.disableSubnetIntraNetwork = subnet.options.disableSubnetIntraNetwork;
+    newSubnet.subnetType = subnet.options.subnetType;
+
+    return newSubnet;
+  }
+
+  updateNetworkingRules(subnet: Subnet, allowConnections: boolean): void {
+    const childrenDependencies = this.getChildren('subnet');
+    if (!childrenDependencies['subnet']) childrenDependencies['subnet'] = [];
+    const subnets = childrenDependencies['subnet'].map((d) => d.to);
+
+    if (allowConnections && !subnets.find((s: Subnet) => s.subnetId === subnet.subnetId)) {
+      this.addChild('subnetId', subnet, 'subnetId');
+    } else if (!allowConnections && subnets.find((s: Subnet) => s.subnetId === subnet.subnetId)) {
+      this.removeRelationship(subnet);
+    }
   }
 }
