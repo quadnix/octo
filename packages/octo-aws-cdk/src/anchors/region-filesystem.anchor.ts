af6a2fc94a09f913416996387301a4ff59550475
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-05-27 13:32:43 -0400

    patch(feat): octo-aws-cdk anchors must synth properties.

--- packages/octo-aws-cdk/src/anchors/region-filesystem.anchor.ts
+++ packages/octo-aws-cdk/src/anchors/region-filesystem.anchor.ts
@@ -1,9 +1,47 @@
-import { AAnchor, Anchor } from '@quadnix/octo';
+import { AAnchor, Anchor, IAnchor, UnknownModel } from '@quadnix/octo';
 import { AwsRegion } from '../models/region/aws.region.model.js';
+
+interface IRegionFilesystemAnchor extends IAnchor {
+  filesystemName: string;
+}
 
 @Anchor()
 export class RegionFilesystemAnchor extends AAnchor {
-  constructor(anchorId: string, parent: AwsRegion) {
+  readonly filesystemName: string;
+
+  constructor(anchorId: string, filesystemName: string, parent: AwsRegion) {
     super(anchorId, parent);
+    this.filesystemName = filesystemName;
+  }
+
+  override synth(): IRegionFilesystemAnchor {
+    return {
+      anchorId: this.anchorId,
+      filesystemName: this.filesystemName,
+      parent: { context: this.getParent().getContext() },
+    };
+  }
+
+  override toJSON(): object {
+    return {
+      anchorId: this.anchorId,
+      filesystemName: this.filesystemName,
+      parent: this.getParent().getContext(),
+    };
+  }
+
+  static override async unSynth(
+    deserializationClass: any,
+    anchor: IRegionFilesystemAnchor,
+    deReferenceContext: (context: string) => Promise<UnknownModel>,
+  ): Promise<RegionFilesystemAnchor> {
+    const parent = await deReferenceContext(anchor.parent.context);
+    const newAnchor = parent.getAnchor(anchor.anchorId) as RegionFilesystemAnchor;
+    if (!newAnchor) {
+      const deserializedAnchor = new deserializationClass(anchor.anchorId, parent);
+      deserializedAnchor.filesystemName = anchor.filesystemName;
+      return deserializedAnchor;
+    }
+    return newAnchor;
   }
 }
--- packages/octo-aws-cdk/src/anchors/security-group.anchor.ts
+++ packages/octo-aws-cdk/src/anchors/security-group.anchor.ts
@@ -1,6 +1,10 @@
-import { AAnchor, Anchor } from '@quadnix/octo';
+import { AAnchor, Anchor, IAnchor, UnknownModel } from '@quadnix/octo';
 import { AwsExecution } from '../models/execution/aws.execution.model.js';
 import { AwsServer } from '../models/server/aws.server.model.js';
+
+interface ISecurityGroupAnchor extends IAnchor {
+  rules: ISecurityGroupAnchorRule[];
+}
 
 interface ISecurityGroupAnchorRule {
   CidrBlock: string;
@@ -17,5 +21,36 @@ export class SecurityGroupAnchor extends AAnchor {
   constructor(anchorId: string, rules: ISecurityGroupAnchorRule[], parent: AwsServer | AwsExecution) {
     super(anchorId, parent);
     this.rules = rules;
+  }
+
+  override synth(): ISecurityGroupAnchor {
+    return {
+      anchorId: this.anchorId,
+      parent: { context: this.getParent().getContext() },
+      rules: [...this.rules],
+    };
+  }
+
+  override toJSON(): object {
+    return {
+      anchorId: this.anchorId,
+      parent: this.getParent().getContext(),
+      rules: [...this.rules],
+    };
+  }
+
+  static override async unSynth(
+    deserializationClass: any,
+    anchor: ISecurityGroupAnchor,
+    deReferenceContext: (context: string) => Promise<UnknownModel>,
+  ): Promise<SecurityGroupAnchor> {
+    const parent = await deReferenceContext(anchor.parent.context);
+    const newAnchor = parent.getAnchor(anchor.anchorId) as SecurityGroupAnchor;
+    if (!newAnchor) {
+      const deserializedAnchor = new deserializationClass(anchor.anchorId, parent);
+      deserializedAnchor.rules = anchor.rules;
+      return deserializedAnchor;
+    }
+    return newAnchor;
   }
 }
