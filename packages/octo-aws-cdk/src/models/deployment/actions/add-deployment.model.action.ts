4d1eecc5d4c53ee36fa56b879083eee8876e8a52
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-06-08 06:57:03 -0400

    patch(feat): octo-aws-cdk | OctoAwsDeployment module WIP.
    - Merging a new module but its a work in progress.
    - Not exported in index yet.

--- packages/octo-aws-cdk/src/models/deployment/actions/add-deployment.model.action.ts
+++ packages/octo-aws-cdk/src/models/deployment/actions/add-deployment.model.action.ts
@@ -1,72 +1,25 @@
-import {
-  Action,
-  type ActionInputs,
-  type ActionOutputs,
-  Diff,
-  DiffAction,
-  Factory,
-  type IModelAction,
-  ModelType,
-} from '@quadnix/octo';
-import { join, resolve } from 'path';
-import type { S3Storage } from '../../../resources/s3/storage/s3-storage.resource.js';
-import { FileUtility } from '../../../utilities/file/file.utility.js';
-import type { AwsServer } from '../../server/aws.server.model.js';
-import type { AwsDeployment } from '../aws.deployment.model.js';
+import { Action, type ActionOutputs, Diff, DiffAction, Factory, type IModelAction, ModelType } from '@quadnix/octo';
+import { AwsDeployment } from '../aws.deployment.model.js';
 
 @Action(ModelType.MODEL)
 export class AddDeploymentModelAction implements IModelAction {
   readonly ACTION_NAME: string = 'AddDeploymentModelAction';
 
-  collectInput(diff: Diff): string[] {
-    const awsDeployment = diff.model as AwsDeployment;
-
-    const parents = awsDeployment.getParents();
-    const parent = (parents['server'] || parents['support'])[0].to as AwsServer;
-
-    return [
-      `input.server.${parent.serverKey}.deployment.${awsDeployment.deploymentTag}.deploymentFolderLocalPath`,
-      `resource.bucket-${awsDeployment.s3StorageService.bucketName}`,
-    ];
+  collectInput(): string[] {
+    return [];
   }
 
   filter(diff: Diff): boolean {
-    return diff.action === DiffAction.ADD && diff.model.MODEL_NAME === 'deployment' && diff.field === 'deploymentTag';
+    return (
+      diff.action === DiffAction.ADD &&
+      diff.model instanceof AwsDeployment &&
+      diff.model.MODEL_NAME === 'deployment' &&
+      diff.field === 'deploymentTag'
+    );
   }
 
-  async handle(diff: Diff, actionInputs: ActionInputs): Promise<ActionOutputs> {
-    const awsDeployment = diff.model as AwsDeployment;
-
-    const parents = awsDeployment.getParents();
-    const parent = (parents['server'] || parents['support'])[0].to as AwsServer;
-
-    const deploymentFolderLocalPath = actionInputs[
-      `input.server.${parent.serverKey}.deployment.${awsDeployment.deploymentTag}.deploymentFolderLocalPath`
-    ] as string;
-    const s3Storage = actionInputs[`resource.bucket-${awsDeployment.s3StorageService.bucketName}`] as S3Storage;
-
-    // Update manifest with contents of local deployment folder.
-    const sourcePaths: { action: 'add'; localPath: string; remotePath: string }[] = [];
-    const resolvedDeploymentFolderLocalPath = resolve(deploymentFolderLocalPath);
-    const filePaths = await FileUtility.readDirectoryRecursively(resolvedDeploymentFolderLocalPath);
-    for (const filePath of filePaths) {
-      sourcePaths.push({
-        action: 'add',
-        localPath: join(resolvedDeploymentFolderLocalPath, filePath),
-        remotePath: `${awsDeployment.deploymentFolderRemotePath}/${filePath}`,
-      });
-    }
-    s3Storage.updateManifestDiff(
-      sourcePaths.reduce((accumulator, currentValue) => {
-        accumulator[currentValue.remotePath] = [currentValue.action, currentValue.localPath];
-        return accumulator;
-      }, {}),
-    );
-
-    const output: ActionOutputs = {};
-    output[s3Storage.resourceId] = s3Storage;
-
-    return output;
+  async handle(): Promise<ActionOutputs> {
+    return {};
   }
 
   async revert(): Promise<ActionOutputs> {
--- packages/octo-aws-cdk/src/models/deployment/actions/delete-deployment.model.action.ts
+++ packages/octo-aws-cdk/src/models/deployment/actions/delete-deployment.model.action.ts
@@ -1,55 +1,25 @@
-import {
-  Action,
-  type ActionInputs,
-  type ActionOutputs,
-  Diff,
-  DiffAction,
-  Factory,
-  type IModelAction,
-  ModelType,
-} from '@quadnix/octo';
-import type { S3Storage } from '../../../resources/s3/storage/s3-storage.resource.js';
-import type { AwsDeployment } from '../aws.deployment.model.js';
+import { Action, type ActionOutputs, Diff, DiffAction, Factory, type IModelAction, ModelType } from '@quadnix/octo';
+import { AwsDeployment } from '../aws.deployment.model.js';
 
 @Action(ModelType.MODEL)
 export class DeleteDeploymentModelAction implements IModelAction {
   readonly ACTION_NAME: string = 'DeleteDeploymentModelAction';
 
-  collectInput(diff: Diff): string[] {
-    const awsDeployment = diff.model as AwsDeployment;
-
-    return [`resource.bucket-${awsDeployment.s3StorageService.bucketName}`];
+  collectInput(): string[] {
+    return [];
   }
 
   filter(diff: Diff): boolean {
     return (
-      diff.action === DiffAction.DELETE && diff.model.MODEL_NAME === 'deployment' && diff.field === 'deploymentTag'
+      diff.action === DiffAction.DELETE &&
+      diff.model instanceof AwsDeployment &&
+      diff.model.MODEL_NAME === 'deployment' &&
+      diff.field === 'deploymentTag'
     );
   }
 
-  async handle(diff: Diff, actionInputs: ActionInputs): Promise<ActionOutputs> {
-    const awsDeployment = diff.model as AwsDeployment;
-
-    const s3Storage = actionInputs[`resource.bucket-${awsDeployment.s3StorageService.bucketName}`] as S3Storage;
-
-    // Update manifest with directories to delete.
-    const sourcePaths: { action: 'deleteDirectory'; localPath: string; remotePath: string }[] = [];
-    sourcePaths.push({
-      action: 'deleteDirectory',
-      localPath: '',
-      remotePath: awsDeployment.deploymentFolderRemotePath,
-    });
-    s3Storage.updateManifestDiff(
-      sourcePaths.reduce((accumulator, currentValue) => {
-        accumulator[currentValue.remotePath] = [currentValue.action, currentValue.localPath];
-        return accumulator;
-      }, {}),
-    );
-
-    const output: ActionOutputs = {};
-    output[s3Storage.resourceId] = s3Storage;
-
-    return output;
+  async handle(): Promise<ActionOutputs> {
+    return {};
   }
 
   async revert(): Promise<ActionOutputs> {
