942a95d63a1e91a42850caa537165800fb3fb06c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-06-16 19:49:14 -0400

    patch(feat): octo-aws-cdk | ecs-task-definition resource with ability to modify Efs parents.

--- packages/octo-aws-cdk/src/resources/ecs/ecs-task-definition.resource.ts
+++ packages/octo-aws-cdk/src/resources/ecs/ecs-task-definition.resource.ts
@@ -1,5 +1,5 @@
-import { AResource, Resource } from '@quadnix/octo';
-import type { Efs } from '../efs/efs.resource.js';
+import { AResource, DependencyRelationship, Diff, DiffAction, Resource } from '@quadnix/octo';
+import { Efs } from '../efs/efs.resource.js';
 import type { IamRole } from '../iam/iam-role.resource.js';
 import type { IEcsTaskDefinitionProperties, IEcsTaskDefinitionResponse } from './ecs-task-definition.interface.js';
 
@@ -12,6 +12,37 @@ export class EcsTaskDefinition extends AResource<EcsTaskDefinition> {
 
   constructor(resourceId: string, properties: IEcsTaskDefinitionProperties, parents: [IamRole, ...Efs[]]) {
     super(resourceId, properties, parents);
+    this.updateTaskDefinitionEfs(parents.filter((p) => p instanceof Efs) as Efs[]);
+  }
+
+  override async diff(previous: EcsTaskDefinition): Promise<Diff[]> {
+    const diffs = await super.diff(previous);
+
+    // Consolidate all Efs parent updates into a single UPDATE diff.
+    let shouldConsolidateEfsDiffs = false;
+    for (let i = diffs.length - 1; i >= 0; i--) {
+      if (diffs[i].model instanceof Efs && diffs[i].field === 'parent') {
+        shouldConsolidateEfsDiffs = true;
+        diffs.splice(i, 1);
+      }
+    }
+    if (shouldConsolidateEfsDiffs) {
+      diffs.push(new Diff(this, DiffAction.UPDATE, 'resourceId', ''));
+    }
+
+    return diffs;
+  }
+
+  updateTaskDefinitionEfs(efsParents: Efs[]): void {
+    const previousEfsParents = this.getParents('efs')['efs']?.map((d): Efs => d.to as Efs) || [];
+    for (const efsParent of previousEfsParents) {
+      efsParent.removeRelationship(this);
+    }
+    for (const efsParent of efsParents) {
+      efsParent.addChild('resourceId', this, 'resourceId');
+      const dependency = this.getDependency(efsParent, DependencyRelationship.CHILD);
+      dependency?.addBehavior('resourceId', DiffAction.UPDATE, 'resourceId', DiffAction.ADD);
+    }
   }
 
   updateTaskDefinitionEnvironmentVariables(
