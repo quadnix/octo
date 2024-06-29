21d43a1641b1c418156ab09943833e36187071c3
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-06-29 00:38:58 -0400

    patch(feat): octo | support for more detailed state-management service.

--- packages/octo/src/services/state-management/local.state-provider.spec.ts
+++ packages/octo/src/services/state-management/local.state-provider.spec.ts
@@ -2,6 +2,12 @@ import { unlink } from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { promisify } from 'util';
+import { TestResource } from '../../../test/helpers/test-classes.js';
+import { Container } from '../../decorators/container.js';
+import { App } from '../../models/app/app.model.js';
+import { ResourceDataRepository } from '../../resources/resource-data.repository.js';
+import { ModelSerializationService } from '../serialization/model/model-serialization.service.js';
+import { ResourceSerializationService } from '../serialization/resource/resource-serialization.service.js';
 import { LocalStateProvider } from './local.state-provider.js';
 import { StateManagementService } from './state-management.service.js';
 
@@ -9,34 +15,140 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const unlinkAsync = promisify(unlink);
 
 describe('LocalStateProvider UT', () => {
-  let filePath;
+  describe('getModelState()', () => {
+    let filePath;
 
-  afterEach(async () => {
-    if (filePath) {
-      await unlinkAsync(filePath);
-    }
+    afterEach(async () => {
+      if (filePath) {
+        await unlinkAsync(filePath);
+      }
+    });
+
+    it('should be able to get a default state', async () => {
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
+
+      const { data, userData } = await stateManagementService.getModelState('models1.json');
+      expect(data).toMatchInlineSnapshot(`
+       {
+         "anchors": [],
+         "dependencies": [],
+         "models": {},
+         "overlays": [],
+       }
+      `);
+      expect(userData).toMatchInlineSnapshot(`{}`);
+    });
+
+    it('should be able to retrieve a frozen state', async () => {
+      filePath = join(__dirname, 'models2.json');
+
+      const app = new App('test');
+      const modelSerializationService = await Container.get(ModelSerializationService);
+      const serializedOutput = await modelSerializationService.serialize(app);
+
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
+
+      await stateManagementService.saveModelState('models2.json', serializedOutput, { version: 1 });
+      const { data, userData } = await stateManagementService.getModelState('models2.json');
+
+      expect(() => {
+        data['key1'] = 'value1';
+      }).toThrow();
+      expect(() => {
+        userData['key2'] = 'value2';
+      }).toThrow();
+    });
   });
 
-  it('should be able to save state', async () => {
-    filePath = join(__dirname, 'manifest.json');
+  describe('getResourceState()', () => {
+    let filePath;
 
-    const localStateProvider = new LocalStateProvider(__dirname);
-    const stateManagementService = new StateManagementService(localStateProvider);
+    afterEach(async () => {
+      if (filePath) {
+        await unlinkAsync(filePath);
+      }
+    });
 
-    const data = Buffer.from(
-      JSON.stringify({
-        key1: 'value1',
-        key2: 'value2',
-      }),
-    );
-    await stateManagementService.saveState('manifest.json', data);
+    it('should be able to get a default state', async () => {
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
 
-    const manifestData = await stateManagementService.getState('manifest.json');
-    expect(JSON.parse(manifestData.toString())).toMatchInlineSnapshot(`
+      const { data, userData } = await stateManagementService.getResourceState('resources1.json');
+      expect(data).toMatchInlineSnapshot(`
+       {
+         "dependencies": [],
+         "resources": {},
+         "sharedResources": {},
+       }
+      `);
+      expect(userData).toMatchInlineSnapshot(`{}`);
+    });
+
+    it('should be able to retrieve a frozen state', async () => {
+      filePath = join(__dirname, 'resources2.json');
+
+      const resource1 = new TestResource('resource-1');
+      const resourceDataRepository = await Container.get(ResourceDataRepository);
+      resourceDataRepository.add(resource1);
+
+      const resourceSerializationService = await Container.get(ResourceSerializationService);
+      const serializedOutput = await resourceSerializationService.serialize();
+
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
+
+      await stateManagementService.saveResourceState('resources2.json', serializedOutput, { version: 1 });
+      const { data, userData } = await stateManagementService.getModelState('resources2.json');
+
+      expect(() => {
+        data['key1'] = 'value1';
+      }).toThrow();
+      expect(() => {
+        userData['key2'] = 'value2';
+      }).toThrow();
+    });
+  });
+
+  describe('getState()', () => {
+    let filePath;
+
+    afterEach(async () => {
+      if (filePath) {
+        await unlinkAsync(filePath);
+      }
+    });
+
+    it('should be able to get a default state', async () => {
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
+
+      const state = await stateManagementService.getState('manifest1.json', 'default');
+      expect(state).toMatchInlineSnapshot(`"default"`);
+    });
+
+    it('should be able to get the saved state', async () => {
+      filePath = join(__dirname, 'manifest2.json');
+
+      const localStateProvider = new LocalStateProvider(__dirname);
+      const stateManagementService = new StateManagementService(localStateProvider);
+
+      const data = Buffer.from(
+        JSON.stringify({
+          key1: 'value1',
+          key2: 'value2',
+        }),
+      );
+      await stateManagementService.saveState('manifest2.json', data);
+
+      const manifestData = await stateManagementService.getState('manifest2.json');
+      expect(JSON.parse(manifestData.toString())).toMatchInlineSnapshot(`
       {
         "key1": "value1",
         "key2": "value2",
       }
     `);
+    });
   });
 });
--- packages/octo/src/services/state-management/state-management.service.ts
+++ packages/octo/src/services/state-management/state-management.service.ts
@@ -1,3 +1,4 @@
+import { ModelSerializedOutput, ResourceSerializedOutput } from '../../app.type.js';
 import { Factory } from '../../decorators/factory.decorator.js';
 import type { IStateProvider } from './state-provider.interface.js';
 
@@ -6,6 +7,57 @@ export class StateManagementService {
 
   constructor(stateProvider: IStateProvider) {
     this.stateProvider = stateProvider;
+  }
+
+  async getModelState(
+    stateFileName: string,
+    { freeze = true }: { freeze?: boolean } = {},
+  ): Promise<{ data: ModelSerializedOutput; userData: object }> {
+    const defaultValue: { data: ModelSerializedOutput; userData: object } = {
+      data: {
+        anchors: [],
+        dependencies: [],
+        models: {},
+        overlays: [],
+      },
+      userData: {},
+    };
+
+    try {
+      const modelState = await this.getState(stateFileName);
+      return JSON.parse(modelState.toString(), (k, v) => (v && freeze ? Object.freeze(v) : v));
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        return defaultValue;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async getResourceState(
+    stateFileName: string,
+    { freeze = true }: { freeze?: boolean } = {},
+  ): Promise<{ data: ResourceSerializedOutput; userData: object }> {
+    const defaultValue: { data: ResourceSerializedOutput; userData: object } = {
+      data: {
+        dependencies: [],
+        resources: {},
+        sharedResources: {},
+      },
+      userData: {},
+    };
+
+    try {
+      const resourceState = await this.getState(stateFileName);
+      return JSON.parse(resourceState.toString(), (k, v) => (v && freeze ? Object.freeze(v) : v));
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        return defaultValue;
+      } else {
+        throw error;
+      }
+    }
   }
 
   async getState(stateFileName: string, defaultValue?: any): Promise<Buffer> {
@@ -18,6 +70,14 @@ export class StateManagementService {
         throw error;
       }
     }
+  }
+
+  async saveModelState(stateFileName: string, data: ModelSerializedOutput, userData: object): Promise<void> {
+    await this.saveState(stateFileName, Buffer.from(JSON.stringify({ data, userData })));
+  }
+
+  async saveResourceState(stateFileName: string, data: ResourceSerializedOutput, userData: object): Promise<void> {
+    await this.saveState(stateFileName, Buffer.from(JSON.stringify({ data, userData })));
   }
 
   async saveState(stateFileName: string, data: Buffer): Promise<void> {
