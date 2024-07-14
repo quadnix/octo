532be5ceb18b53af5b7cfb18aa48a2c369c64d71
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-07-14 16:19:06 -0400

    patch(feat): octo | RetryUtility with ability to change defaults.

--- packages/octo-aws-cdk/src/utilities/retry/retry.utility.ts
+++ packages/octo-aws-cdk/src/utilities/retry/retry.utility.ts
@@ -7,14 +7,20 @@ interface IRetryOptions {
 }
 
 export class RetryUtility {
+  private static DEFAULT_BACK_OFF_FACTOR = 1;
+  private static DEFAULT_INITIAL_DELAY_IN_MS = 10000;
+  private static DEFAULT_MAX_RETRIES = 3;
+  private static DEFAULT_RETRY_DELAY_IN_MS = 10000;
+  private static DEFAULT_THROW_ON_ERROR = true;
+
   static async retryPromise(
     operation: () => Promise<boolean>,
     {
-      backOffFactor = 1,
-      initialDelayInMs = 10000,
-      maxRetries = 3,
-      retryDelayInMs = 10000,
-      throwOnError = true,
+      backOffFactor = RetryUtility.DEFAULT_BACK_OFF_FACTOR,
+      initialDelayInMs = RetryUtility.DEFAULT_INITIAL_DELAY_IN_MS,
+      maxRetries = RetryUtility.DEFAULT_MAX_RETRIES,
+      retryDelayInMs = RetryUtility.DEFAULT_RETRY_DELAY_IN_MS,
+      throwOnError = RetryUtility.DEFAULT_THROW_ON_ERROR,
     }: IRetryOptions = {},
   ): Promise<void> {
     await new Promise((resolve) => setTimeout(resolve, initialDelayInMs));
@@ -39,6 +45,24 @@ export class RetryUtility {
 
     if (throwOnError) {
       throw new Error('Exhausted all retries for the operation!');
+    }
+  }
+
+  static setDefaults(options: Partial<IRetryOptions>): void {
+    if (options.backOffFactor !== undefined) {
+      this.DEFAULT_BACK_OFF_FACTOR = options.backOffFactor;
+    }
+    if (options.initialDelayInMs !== undefined) {
+      this.DEFAULT_INITIAL_DELAY_IN_MS = options.initialDelayInMs;
+    }
+    if (options.maxRetries !== undefined) {
+      this.DEFAULT_MAX_RETRIES = options.maxRetries;
+    }
+    if (options.retryDelayInMs !== undefined) {
+      this.DEFAULT_RETRY_DELAY_IN_MS = options.retryDelayInMs;
+    }
+    if (options.throwOnError !== undefined) {
+      this.DEFAULT_THROW_ON_ERROR = options.throwOnError;
     }
   }
 }
