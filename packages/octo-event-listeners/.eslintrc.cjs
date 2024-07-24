1d6e2117e58806cc5c685134031444f081eebab9
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-07-23 20:17:50 -0400

    patch(feat): octo-event-listeners | new library for event common event listeners.

---
+++ packages/octo-event-listeners/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['../../.eslintrc.cjs'],
+  ignorePatterns: ['coverage', 'dist', 'node_modules'],
+  parserOptions: {
+    ecmaVersion: '2022',
+    extraFileExtensions: ['.json'],
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  rules: {
+    '@nx/enforce-module-boundaries': [
+      'error',
+      {
+        allow: [],
+        depConstraints: [
+          {
+            onlyDependOnLibsWithTags: ['scope:octo', 'scope:octo-event-listeners'],
+            sourceTag: 'scope:octo-event-listeners',
+          },
+        ],
+      },
+    ],
+  },
+};
