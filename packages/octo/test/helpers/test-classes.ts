d0fec1ed78e4645d951e20e1c9c8eba86c92b85c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-11-21 07:09:21 -0500

    patch(chore): octo | test-classes reviewed.

--- packages/octo/test/helpers/test-classes.ts
+++ packages/octo/test/helpers/test-classes.ts
@@ -1,34 +1,33 @@
+import { NodeType, type UnknownAnchor, type UnknownModel, type UnknownResource } from '../../src/app.type.js';
 import {
   AAnchor,
-  AModel,
   AOverlay,
   AResource,
   ASharedResource,
+  type BaseAnchorSchema,
+  type BaseOverlaySchema,
+  type BaseResourceSchema,
   type Diff,
-  type IAnchor,
-  type IOverlay,
-  type IResource,
-  NodeType,
-  type UnknownModel,
-  type UnknownResource,
 } from '../../src/index.js';
+import { AModel } from '../../src/models/model.abstract.js';
 
-export class SharedTestResource extends ASharedResource<TestResource> {
+export class SharedTestResource extends ASharedResource<BaseResourceSchema, TestResource> {
   static override readonly NODE_NAME: string = 'test-resource';
   static override readonly NODE_PACKAGE: string = '@octo';
+  static override readonly NODE_SCHEMA = {};
   static override readonly NODE_TYPE: NodeType = NodeType.SHARED_RESOURCE;
 
   constructor(resourceId: string, properties: { [key: string]: unknown }, parents: TestResource[]) {
-    super(resourceId, properties, parents as AResource<TestResource>[]);
+    super(resourceId, properties, parents as AResource<BaseResourceSchema, TestResource>[]);
   }
 }
 
 export class TestAction {}
 
-export class TestAnchor extends AAnchor {
+export class TestAnchor extends AAnchor<BaseAnchorSchema, UnknownModel> {
   static override readonly NODE_PACKAGE: string = '@octo';
 
-  constructor(anchorId: string, properties: IAnchor['properties'], parent: UnknownModel) {
+  constructor(anchorId: string, properties: BaseAnchorSchema['properties'], parent: UnknownModel) {
     super(anchorId, properties, parent);
   }
 }
@@ -36,6 +35,7 @@ export class TestAnchor extends AAnchor {
 export class TestModelWithoutUnsynth extends AModel<object, TestModelWithoutUnsynth> {
   static override readonly NODE_NAME: string = 'test-model';
   static override readonly NODE_PACKAGE: string = '@octo';
+  static override readonly NODE_SCHEMA = {};
   static override readonly NODE_TYPE: NodeType = NodeType.MODEL;
 
   override async diffProperties(): Promise<Diff[]> {
@@ -51,32 +51,39 @@ export class TestModelWithoutUnsynth extends AModel<object, TestModelWithoutUnsy
   }
 }
 
-export class TestOverlay extends AOverlay<TestOverlay> {
+export class TestOverlay extends AOverlay<BaseOverlaySchema, TestOverlay> {
   static override readonly NODE_NAME: string = 'test-overlay';
   static override readonly NODE_PACKAGE: string = '@octo';
+  static override readonly NODE_SCHEMA = {};
   static override readonly NODE_TYPE: NodeType = NodeType.OVERLAY;
 
-  constructor(overlayId: IOverlay['overlayId'], properties: IOverlay['properties'], anchors: AAnchor[]) {
+  constructor(
+    overlayId: BaseOverlaySchema['overlayId'],
+    properties: BaseOverlaySchema['properties'],
+    anchors: UnknownAnchor[],
+  ) {
     super(overlayId, properties, anchors);
   }
 }
 
-export class TestResource extends AResource<TestResource> {
+export class TestResource extends AResource<BaseResourceSchema, TestResource> {
   static override readonly NODE_NAME: string = 'test-resource';
   static override readonly NODE_PACKAGE: string = '@octo';
+  static override readonly NODE_SCHEMA = {};
   static override readonly NODE_TYPE: NodeType = NodeType.RESOURCE;
 
-  constructor(resourceId: string, properties: IResource['properties'] = {}, parents: UnknownResource[] = []) {
+  constructor(resourceId: string, properties: BaseResourceSchema['properties'] = {}, parents: UnknownResource[] = []) {
     super(resourceId, properties, parents);
   }
 }
 
-export class TestResourceWithDiffOverride extends AResource<TestResource> {
+export class TestResourceWithDiffOverride extends AResource<BaseResourceSchema, TestResource> {
   static override readonly NODE_NAME: string = 'test-resource';
   static override readonly NODE_PACKAGE: string = '@octo';
+  static override readonly NODE_SCHEMA = {};
   static override readonly NODE_TYPE: NodeType = NodeType.RESOURCE;
 
-  constructor(resourceId: string, properties: IResource['properties'] = {}, parents: UnknownResource[] = []) {
+  constructor(resourceId: string, properties: BaseResourceSchema['properties'] = {}, parents: UnknownResource[] = []) {
     super(resourceId, properties, parents);
   }
 
