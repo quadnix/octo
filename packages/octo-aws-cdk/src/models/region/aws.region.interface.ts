09c9f853dd5f2a94eb05eda148880b7dc59f3bdb
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-11-30 17:25:54 -0500

    patch(feat): octo-aws-cdk | removed old region model.

--- packages/octo-aws-cdk/src/models/region/aws.region.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { IRegion } from '@quadnix/octo';
-import type { AwsRegion } from './aws.region.model.js';
-
-export interface IAwsRegion extends IRegion {
-  awsRegionAZ: AwsRegion['awsRegionAZ'];
-  awsRegionId: AwsRegion['awsRegionId'];
-  filesystems: AwsRegion['filesystems'];
-}
