ea489cb97fda9a8e50c1fa9f87f3644732ea8a57
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-12-30 07:38:13 -0500

    patch(feat): octo-aws-cdk | removed old subnet-filesystem-mount overlay.

--- packages/octo-aws-cdk/src/overlays/subnet-filesystem-mount/subnet-filesystem-mount.overlay.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { IOverlay, ModifyInterface } from '@quadnix/octo';
-
-export interface ISubnetFilesystemMountOverlayProperties
-  extends ModifyInterface<IOverlay['properties'], Record<never, never>> {}
