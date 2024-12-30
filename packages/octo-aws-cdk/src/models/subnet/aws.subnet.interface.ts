23ec77099f750c0994309509ae043be73a78590e
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2024-12-30 07:37:51 -0500

    patch(feat): octo-aws-cdk | removed old subnet model.

--- packages/octo-aws-cdk/src/models/subnet/aws.subnet.interface.ts
+++
@@ -1,6 +0,0 @@
-import type { ISubnet } from '@quadnix/octo';
-import type { AwsSubnet } from './aws.subnet.model.js';
-
-export interface IAwsSubnet extends ISubnet {
-  filesystemMounts: AwsSubnet['filesystemMounts'];
-}
