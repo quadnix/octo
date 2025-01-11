d1f1e7ebe8bd47c7bfd5fe1e54fa608043420310
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-01-11 09:20:29 -0500

    patch(feat): octo-aws-cdk | removed old s3-storage-access overlay.

--- packages/octo-aws-cdk/src/overlays/s3-storage-access/s3-storage-access.overlay.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { IOverlay, ModifyInterface } from '@quadnix/octo';
-
-export interface IS3StorageAccessOverlayProperties
-  extends ModifyInterface<IOverlay['properties'], { iamRolePolicyName: string }> {}
