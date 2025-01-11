9c323d4526fce53570a8ddd2efc9d5997a350f56
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-01-11 10:34:08 -0500

    patch(feat): octo | removed old s3-storage model and anchors.

--- packages/octo-aws-cdk/src/models/service/s3-storage/s3-storage.service.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { IService } from '@quadnix/octo';
-import type { S3StorageService } from './s3-storage.service.model.js';
-
-export interface IS3StorageService extends IService {
-  awsRegionId: S3StorageService['awsRegionId'];
-  bucketName: S3StorageService['bucketName'];
-  directories: S3StorageService['directories'];
-}
