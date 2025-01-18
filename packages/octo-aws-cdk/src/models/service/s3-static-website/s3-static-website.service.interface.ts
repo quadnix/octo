872cb3f44b3773cd9a08ee4717f8f6f25c1368ee
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-01-17 22:03:14 -0500

    patch(feat): octo-aws-cdk | removed old s3-website model and modules.

--- packages/octo-aws-cdk/src/models/service/s3-static-website/s3-static-website.service.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { IService } from '@quadnix/octo';
-import type { S3StaticWebsiteService } from './s3-static-website.service.model.js';
-
-export interface IS3StaticWebsiteService extends IService {
-  awsRegionId: S3StaticWebsiteService['awsRegionId'];
-  bucketName: S3StaticWebsiteService['bucketName'];
-  excludePaths: S3StaticWebsiteService['excludePaths'];
-  sourcePaths: S3StaticWebsiteService['sourcePaths'];
-}
