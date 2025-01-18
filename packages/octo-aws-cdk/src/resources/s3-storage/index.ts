debb25e0fa053d14096c3988e757b81146a2b83c
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-01-17 22:01:55 -0500

    patch(fix): octo-aws-cdk | s3-storage resource does not update.
    The S3 storage resource should not support uploading of files.
    Its responsibility is only to create a S3 bucket for storage.
    Supporting files in S3 is a product in itself, and that is not
    what Octo is setup to do. Its an infrastructure, so it should only
    create infrastructure.
    
    S3 website is a different concept, as it manages the website.
    S3 storage is not the same as that.
    S3 storage is more similar to ECR in this regard.

--- packages/octo-aws-cdk/src/resources/s3-storage/index.ts
+++ packages/octo-aws-cdk/src/resources/s3-storage/index.ts
@@ -1,6 +1,5 @@
 import './actions/add-s3-storage.resource.action.js';
 import './actions/delete-s3-storage.resource.action.js';
-import './actions/update-source-paths-in-s3-storage.resource.action.js';
 
 export { S3Storage } from './s3-storage.resource.js';
 export { S3StorageSchema } from './s3-storage.schema.js';
--- packages/octo-aws-cdk/src/resources/s3-storage/s3-storage.resource.ts
+++ packages/octo-aws-cdk/src/resources/s3-storage/s3-storage.resource.ts
@@ -1,4 +1,4 @@
-import { AResource, Diff, DiffAction, Resource } from '@quadnix/octo';
+import { AResource, Resource } from '@quadnix/octo';
 import { S3StorageSchema } from './s3-storage.schema.js';
 
 @Resource<S3Storage>('@octo', 's3-storage', S3StorageSchema)
@@ -6,35 +6,7 @@ export class S3Storage extends AResource<S3StorageSchema, S3Storage> {
   declare properties: S3StorageSchema['properties'];
   declare response: S3StorageSchema['response'];
 
-  private readonly manifestDiff: { [key: string]: ['add' | 'delete' | 'deleteDirectory', string] } = {};
-
   constructor(resourceId: string, properties: S3StorageSchema['properties']) {
     super(resourceId, properties, []);
-  }
-
-  override async diffInverse(diff: Diff, deReferenceResource: (resourceId: string) => Promise<never>): Promise<void> {
-    if (diff.field === 'update-source-paths' && diff.action === DiffAction.UPDATE) {
-      // do nothing, since nothing in node changes for this diff action.
-    } else {
-      await super.diffInverse(diff, deReferenceResource);
-    }
-  }
-
-  override async diffProperties(): Promise<Diff[]> {
-    const diffs: Diff[] = [];
-
-    if (this.manifestDiff && Object.keys(this.manifestDiff).length > 0) {
-      diffs.push(
-        new Diff(this, DiffAction.UPDATE, 'update-source-paths', JSON.parse(JSON.stringify(this.manifestDiff))),
-      );
-    }
-
-    return diffs;
-  }
-
-  updateManifestDiff(manifestDiff: S3Storage['manifestDiff']): void {
-    for (const key of Object.keys(manifestDiff)) {
-      this.manifestDiff[key] = [...manifestDiff[key]];
-    }
   }
 }
