50b5e13d2a38c9a5927f2ffd0acdf5e48ba84bc2
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-01-25 20:58:16 -0500

    patch(feat): octo-aws-cdk | removed old deployment model.

--- packages/octo-aws-cdk/src/models/deployment/aws.deployment.interface.ts
+++
@@ -1,6 +0,0 @@
-import type { IDeployment, IModelReference } from '@quadnix/octo';
-
-export interface IAwsDeployment extends IDeployment {
-  deploymentFolderRemotePath: string;
-  s3StorageService: IModelReference;
-}
