3bca4d73a6d10aab284d9cd96661137be9983aab
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-02-07 08:57:07 -0500

    patch(feat): octo-aws-cdk | eslint and exports fix.

--- packages/octo-aws-cdk/.eslintrc.cjs
+++ packages/octo-aws-cdk/.eslintrc.cjs
@@ -25,12 +25,69 @@ module.exports = {
     'boundaries/element-types': [
       'error',
       {
-        default: 'allow',
+        default: 'disallow',
         rules: [
+          // Anchor rules.
           {
-            allow: ['resources', 'utilities'],
-            disallow: ['models', 'overlays'],
-            from: ['resources'],
+            allow: ['utilities'],
+            from: ['anchors'],
+          },
+
+          // Module rules.
+          {
+            allow: [
+              ['module-*', { family: '${from.family}' }],
+              'anchors',
+              'resource-index',
+              'resource-schema',
+              'utilities',
+            ],
+            from: ['module-actions'],
+          },
+          {
+            allow: [['module-*', { family: '${from.family}' }], 'anchors', 'utilities'],
+            from: ['module-models', 'module-overlays'],
+          },
+          {
+            allow: [['module-*', { family: '${from.family}' }], 'anchors', 'module-schema-index', 'utilities'],
+            from: ['module-module'],
+          },
+          {
+            allow: [
+              ['module-*', { family: '${from.family}' }],
+              'anchors',
+              'module-schema-index',
+              'resource-index',
+              'resource-schema',
+              'utilities',
+            ],
+            from: ['module-spec'],
+          },
+          {
+            allow: ['module-models', 'module-overlays', 'resource-index'],
+            from: ['module-index'],
+          },
+          {
+            allow: [['module-*', { family: '${from.family}' }], 'anchors'],
+            from: ['module-schema-index'],
+          },
+
+          // Resource rules.
+          {
+            allow: [
+              ['resource-resource', { family: '${from.family}' }],
+              ['resource-schema', { family: '${from.family}' }],
+              'utilities',
+            ],
+            from: ['resource-actions'],
+          },
+          {
+            allow: [['resource-schema', { family: '${from.family}' }], 'resource-schema', 'utilities'],
+            from: ['resource-resource'],
+          },
+          {
+            allow: [['resource-actions', { family: '${from.family}' }]],
+            from: ['resource-index'],
           },
         ],
       },
@@ -39,18 +96,80 @@ module.exports = {
   },
   settings: {
     'boundaries/elements': [
+      // Anchor patterns.
       {
-        pattern: 'src/models',
-        type: 'models',
+        capture: ['family'],
+        mode: 'folder',
+        pattern: 'src/anchors/*',
+        type: 'anchors',
+      },
+
+      // Module patterns.
+      {
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: ['src/modules/*/*/**/*.action.ts'],
+        type: 'module-actions',
       },
       {
-        pattern: 'src/overlays',
-        type: 'overlays',
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: ['src/modules/*/*/models/*/*.ts'],
+        type: 'module-models',
       },
       {
-        pattern: 'src/resources',
-        type: 'resources',
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: ['src/modules/*/*/overlays/*/*.ts'],
+        type: 'module-overlays',
       },
+      {
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: 'src/modules/*/*/*.module.ts',
+        type: 'module-module',
+      },
+      {
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: 'src/modules/*/*/*.module.spec.ts',
+        type: 'module-spec',
+      },
+      {
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: 'src/modules/*/*/index.ts',
+        type: 'module-index',
+      },
+      {
+        capture: ['familyCategory', 'family'],
+        mode: 'file',
+        pattern: 'src/modules/*/*/index.schema.ts',
+        type: 'module-schema-index',
+      },
+
+      // Resource patterns.
+      {
+        capture: ['family'],
+        mode: 'folder',
+        pattern: 'src/resources/*/actions',
+        type: `resource-actions`,
+      },
+      {
+        capture: ['family'],
+        mode: 'file',
+        pattern: 'src/resources/*/*.resource.ts',
+        type: `resource-resource`,
+      },
+      {
+        capture: ['family'],
+        mode: 'file',
+        pattern: 'src/resources/*/*.schema.ts',
+        type: `resource-schema`,
+      },
+      { capture: ['family'], mode: 'file', pattern: 'src/resources/*/index.ts', type: `resource-index` },
+
+      // Utility patterns.
       {
         pattern: 'src/utilities',
         type: 'utilities',
