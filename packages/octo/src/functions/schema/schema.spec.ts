1740d88bf25253d263ee960bdc6e285f07f6f2da
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-05-18 08:51:26 -0400

    chore(feat): octo | nested schema validation tests.

--- packages/octo/src/functions/schema/schema.spec.ts
+++ packages/octo/src/functions/schema/schema.spec.ts
@@ -75,6 +75,75 @@ describe('Schema UT', () => {
       const instance3 = getSchemaInstance(TestSchema, value3);
       expect(instance3).toEqual(value3);
     });
+
+    it('should validate optional nested schema', () => {
+      class NestedSchema {
+        @Validate({ destruct: (value?: string): string[] => (value ? [value] : []), options: { minLength: 4 } })
+        name? = Schema<string>();
+      }
+      class TestSchema {
+        @Validate({
+          destruct: (value: NestedSchema): NestedSchema[] => (Object.keys(value).length > 0 ? [value] : []),
+          options: { isSchema: { schema: NestedSchema } },
+        })
+        student? = Schema<NestedSchema>({});
+      }
+
+      const value1 = {};
+      const instance1 = getSchemaInstance(TestSchema, value1);
+      expect(instance1).toEqual({ student: {} });
+
+      const value2 = { student: { name: 'bad' } };
+      expect(() => getSchemaInstance(TestSchema, value2)).toThrow(
+        'Property "student" in schema could not be validated!',
+      );
+
+      const value3 = { student: { name: 'good' } };
+      const instance3 = getSchemaInstance(TestSchema, value3);
+      expect(instance3).toEqual(value3);
+    });
+
+    it('should validate optional property', () => {
+      class TestSchema {
+        @Validate({
+          destruct: (value: { key1?: string }): string[] => {
+            const values: string[] = [];
+            if (value.key1 !== undefined) {
+              values.push(value.key1);
+            }
+            return values;
+          },
+          options: { minLength: 1 },
+        })
+        properties? = Schema<{ key1?: string }>({});
+      }
+
+      const value1 = {};
+      const instance1 = getSchemaInstance(TestSchema, value1);
+      expect(instance1).toEqual({ properties: {} });
+
+      const value2 = { properties: {} };
+      const instance2 = getSchemaInstance(TestSchema, value2);
+      expect(instance2).toEqual({ properties: {} });
+
+      const value3 = { properties: { key1: '' } };
+      expect(() => getSchemaInstance(TestSchema, value3)).toThrow(
+        'Property "properties" in schema could not be validated!',
+      );
+
+      const value4 = { properties: { key1: undefined } };
+      const instance4 = getSchemaInstance(TestSchema, value4);
+      expect(instance4).toEqual({ properties: {} });
+
+      const value5 = { properties: { key1: null } };
+      expect(() => getSchemaInstance(TestSchema, value5 as any)).toThrow(
+        'Property "properties" in schema could not be validated!',
+      );
+
+      const value6 = { properties: { key1: 'value1' } };
+      const instance6 = getSchemaInstance(TestSchema, value6);
+      expect(instance6).toEqual(value6);
+    });
   });
 
   describe('getSchemaKeys()', () => {
