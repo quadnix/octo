399ead4efaf11c2990537d5e113c356acede2503
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-05-20 08:23:10 -0400

    chore(feat): octo | UT for enable-hook.decorator.ts

---
+++ packages/octo/src/decorators/enable-hook.decorator.spec.ts
@@ -0,0 +1,46 @@
+import { jest } from '@jest/globals';
+import { CommitHook } from '../functions/hook/commit.hook.js';
+import { TestContainer } from '../functions/container/test-container.js';
+import { EnableHook } from './enable-hook.decorator.js';
+
+describe('EnableHook UT', () => {
+  beforeEach(async () => {
+    await TestContainer.create({ mocks: [] }, { factoryTimeoutInMs: 500 });
+  });
+
+  afterEach(async () => {
+    await TestContainer.reset();
+
+    jest.restoreAllMocks();
+  });
+
+  it('should throw an error when an invalid hook type is provided', () => {
+    const descriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: true,
+      value: function testMethod() {},
+      writable: true,
+    };
+
+    expect(() => {
+      const decorator = EnableHook('InvalidHook' as 'CommitHook');
+      decorator({}, 'testMethod', descriptor);
+    }).toThrowErrorMatchingInlineSnapshot(`"Invalid hook "InvalidHook"!"`);
+  });
+
+  it('should register a method with CommitHook when "CommitHook" is provided', () => {
+    const descriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: true,
+      value: function testMethod() {},
+      writable: true,
+    };
+    const registrarSpy = jest.spyOn(CommitHook.getInstance(), 'registrar').mockImplementation(() => {});
+
+    const decorator = EnableHook('CommitHook');
+    decorator({}, 'testMethod', descriptor);
+
+    expect(registrarSpy).toHaveBeenCalledTimes(1);
+    expect(registrarSpy).toHaveBeenCalledWith(descriptor);
+  });
+});
