625b0b8ce6b2d3d6e21d7cf877d051d60cdfb61f
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-05-24 23:20:25 -0400

    patch(fix): octo-aws-cdk | vpc to enable dns support.

--- packages/octo-aws-cdk/src/resources/vpc/actions/add-vpc.resource.action.ts
+++ packages/octo-aws-cdk/src/resources/vpc/actions/add-vpc.resource.action.ts
@@ -1,4 +1,4 @@
-import { CreateVpcCommand, EC2Client } from '@aws-sdk/client-ec2';
+import { CreateVpcCommand, EC2Client, ModifyVpcAttributeCommand } from '@aws-sdk/client-ec2';
 import { Action, Container, type Diff, DiffAction, Factory, type IResourceAction } from '@quadnix/octo';
 import type { ECSClientFactory } from '../../../factories/aws-client.factory.js';
 import { Vpc } from '../vpc.resource.js';
@@ -37,6 +37,18 @@ export class AddVpcResourceAction implements IResourceAction<Vpc> {
       }),
     );
 
+    await ec2Client.send(
+      new ModifyVpcAttributeCommand({
+        EnableDnsHostnames: {
+          Value: true,
+        },
+        EnableDnsSupport: {
+          Value: true,
+        },
+        VpcId: vpcOutput.Vpc!.VpcId!,
+      }),
+    );
+
     // Set response.
     response.VpcId = vpcOutput.Vpc!.VpcId!;
   }
@@ -53,6 +65,8 @@ export class AddVpcResourceAction implements IResourceAction<Vpc> {
     ec2Client.send = async (instance: unknown): Promise<unknown> => {
       if (instance instanceof CreateVpcCommand) {
         return { Vpc: { VpcId: capture.VpcId } };
+      } else if (instance instanceof ModifyVpcAttributeCommand) {
+        return {};
       }
     };
   }
