2aa12a0ab81ac152f900f53723894a05d9162744
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2025-06-15 05:54:32 -0400

    patch(feat): octo-aws-cdk | support for endpoint in aws factories.

--- packages/octo-aws-cdk/src/factories/aws-client.factory.ts
+++ packages/octo-aws-cdk/src/factories/aws-client.factory.ts
@@ -8,6 +8,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { STSClient } from '@aws-sdk/client-sts';
 import { Upload } from '@aws-sdk/lib-storage';
 import { Container, Factory } from '@quadnix/octo';
+import type { EndpointInputConfig } from '@smithy/middleware-endpoint/dist-types/resolveEndpointConfig.js';
 import type { AwsCredentialIdentityProvider } from '@smithy/types';
 
 class AwsClientFactory {
@@ -26,13 +27,22 @@ class AwsClientFactory {
     );
     if (instanceIndex === -1) {
       const container = Container.getInstance();
+
       const credentials = await container.get<AwsCredentialIdentityProvider>('AwsCredentialIdentityProvider', {
         metadata: { awsAccountId, package: '@octo' },
       });
+
+      let endpointInputConfig: EndpointInputConfig | undefined;
+      if (container.has('EndpointInputConfig', { metadata: { awsAccountId, package: '@octo' } })) {
+        endpointInputConfig = await container.get<EndpointInputConfig>('EndpointInputConfig', {
+          metadata: { awsAccountId, package: '@octo' },
+        });
+      }
+
       this.instances[this.name].push({
         awsAccountId,
         awsRegionId,
-        value: this.createInstance(awsRegionId, credentials),
+        value: this.createInstance(awsRegionId, credentials, endpointInputConfig),
       });
       instanceIndex = this.instances[this.name].length - 1;
     }
@@ -41,7 +51,7 @@ class AwsClientFactory {
   }
 
   static createInstance(...args: unknown[]): any {
-    if (args.length > 2) {
+    if (args.length > 3) {
       throw new Error('Invalid number of args in createInstance()!');
     }
 
@@ -51,29 +61,45 @@ class AwsClientFactory {
 
 @Factory<EC2Client>(EC2Client, { metadata: { package: '@octo' } })
 export class EC2ClientFactory extends AwsClientFactory {
-  static override createInstance(awsRegionId: string, credentials: AwsCredentialIdentityProvider): EC2Client {
-    return new EC2Client({ ...credentials, region: awsRegionId });
+  static override createInstance(
+    awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): EC2Client {
+    return new EC2Client({ ...credentials, ...endpointInputConfig, region: awsRegionId });
   }
 }
 
 @Factory<ECRClient>(ECRClient, { metadata: { package: '@octo' } })
 export class ECRClientFactory extends AwsClientFactory {
-  static override createInstance(awsRegionId: string, credentials: AwsCredentialIdentityProvider): ECRClient {
-    return new ECRClient({ ...credentials, region: awsRegionId });
+  static override createInstance(
+    awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): ECRClient {
+    return new ECRClient({ ...credentials, ...endpointInputConfig, region: awsRegionId });
   }
 }
 
 @Factory<ECSClient>(ECSClient, { metadata: { package: '@octo' } })
 export class ECSClientFactory extends AwsClientFactory {
-  static override createInstance(awsRegionId: string, credentials: AwsCredentialIdentityProvider): ECSClient {
-    return new ECSClient({ ...credentials, region: awsRegionId });
+  static override createInstance(
+    awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): ECSClient {
+    return new ECSClient({ ...credentials, ...endpointInputConfig, region: awsRegionId });
   }
 }
 
 @Factory<EFSClient>(EFSClient, { metadata: { package: '@octo' } })
 export class EFSClientFactory extends AwsClientFactory {
-  static override createInstance(awsRegionId: string, credentials: AwsCredentialIdentityProvider): EFSClient {
-    return new EFSClient({ ...credentials, region: awsRegionId });
+  static override createInstance(
+    awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): EFSClient {
+    return new EFSClient({ ...credentials, ...endpointInputConfig, region: awsRegionId });
   }
 }
 
@@ -82,8 +108,9 @@ export class ElasticLoadBalancingV2ClientFactory extends AwsClientFactory {
   static override createInstance(
     awsRegionId: string,
     credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
   ): ElasticLoadBalancingV2Client {
-    return new ElasticLoadBalancingV2Client({ ...credentials, region: awsRegionId });
+    return new ElasticLoadBalancingV2Client({ ...credentials, ...endpointInputConfig, region: awsRegionId });
   }
 }
 
@@ -93,15 +120,27 @@ export class IAMClientFactory extends AwsClientFactory {
     return super.create(awsAccountId, 'global');
   }
 
-  static override createInstance(_awsRegionId: string, credentials: AwsCredentialIdentityProvider): IAMClient {
-    return new IAMClient({ ...credentials });
+  static override createInstance(
+    _awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): IAMClient {
+    return new IAMClient({ ...credentials, ...endpointInputConfig });
   }
 }
 
 @Factory<S3Client>(S3Client, { metadata: { package: '@octo' } })
 export class S3ClientFactory extends AwsClientFactory {
-  static override createInstance(awsRegionId: string, credentials: AwsCredentialIdentityProvider): S3Client {
-    return new S3Client({ ...credentials, region: awsRegionId });
+  static override createInstance(
+    awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): S3Client {
+    return new S3Client({
+      ...credentials,
+      ...endpointInputConfig,
+      region: awsRegionId,
+    });
   }
 }
 
@@ -118,7 +157,11 @@ export class STSClientFactory extends AwsClientFactory {
     return super.create(awsAccountId, 'global');
   }
 
-  static override createInstance(_awsRegionId: string, credentials: AwsCredentialIdentityProvider): STSClient {
-    return new STSClient({ ...credentials });
+  static override createInstance(
+    _awsRegionId: string,
+    credentials: AwsCredentialIdentityProvider,
+    endpointInputConfig: EndpointInputConfig = {},
+  ): STSClient {
+    return new STSClient({ ...credentials, ...endpointInputConfig });
   }
 }
