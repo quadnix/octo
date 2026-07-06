97feb3df263adac391fbbf192fb06f6cffd6deaa
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2026-07-05 20:29:52 -0400

    patch(feat): octo-templates | aws-s3-website e2e test with resource tag checks for teardown.

--- packages/octo-templates/src/aws-s3-website/main.e2e-spec.ts
+++ packages/octo-templates/src/aws-s3-website/main.e2e-spec.ts
@@ -4,6 +4,7 @@ import { jest } from '@jest/globals';
 import { type App, TestContainer, TestModuleContainer, TestStateProvider } from '@quadnix/octo';
 import { type AwsS3StaticWebsiteServiceModule } from '@quadnix/octo-aws-cdk/modules/service/aws-s3-static-website-service';
 import axios from 'axios';
+import { AwsTagsUtility } from '../utilities/aws/tags/aws-tags.utility.js';
 import { config } from './app.config.js';
 import { ModuleDefinitions } from './module-definitions.js';
 
@@ -12,6 +13,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const AWS_REGION_ID = 'us-east-1';
 
 const outputDir = join(__dirname, '.octo', 'generated');
+
+const E2E_TAGS = { 'e2e-test': 'true', 'e2e-test-family': 'aws-s3-website' };
 
 jest.setTimeout(600_000);
 
@@ -37,6 +40,9 @@ describe('Main E2E', () => {
       providers: { aws: { minVersion: '5.0.0', source: 'hashicorp/aws' } },
     });
     testModuleContainer.registerTerraformProvider('aws', config.AWS_ACCOUNT_ID, AWS_REGION_ID);
+
+    // Register tags on all resources.
+    testModuleContainer.registerTags([{ scope: {}, tags: E2E_TAGS }]);
 
     ({
       app: [app],
@@ -78,21 +84,28 @@ describe('Main E2E', () => {
     expect(errorContent.data).toContain('This is an error!');
   });
 
-  it('should delete the website and leave octo state empty', async () => {
-    moduleDefinitions.remove('s3-website-service-module');
+  it('should have no resources left after teardown', async () => {
+    for (const moduleId of moduleDefinitions
+      .getAll()
+      .map((md) => md.moduleId)
+      .filter((moduleId) => moduleId !== 'account-module')) {
+      moduleDefinitions.remove(moduleId);
+    }
 
-    const { responses } = (
-      await testModuleContainer
-        .runModules(
-          app,
-          moduleDefinitions.getAll().map((md) => ({ inputs: md.moduleInputs, moduleId: md.moduleId, type: md.module })),
-          { outputDir, terraformTarget: 'apply' },
-        )
-        .next()
-    ).value!;
+    await testModuleContainer
+      .runModules(
+        app,
+        moduleDefinitions.getAll().map((md) => ({ inputs: md.moduleInputs, moduleId: md.moduleId, type: md.module })),
+        { outputDir, terraformTarget: 'apply' },
+      )
+      .next();
 
-    expect(responses).toEqual({});
+    const awsResourcesUtility = new AwsTagsUtility(AWS_REGION_ID);
+    const leftoverArns = await awsResourcesUtility.getResourceArnsByTags(E2E_TAGS);
+    expect(leftoverArns).toEqual([]);
 
-    await expect(axios.get(`http://${bucketName}.s3-website-${AWS_REGION_ID}.amazonaws.com/index.html`)).rejects.toThrow();
+    await expect(
+      axios.get(`http://${bucketName}.s3-website-${AWS_REGION_ID}.amazonaws.com/index.html`),
+    ).rejects.toThrow();
   });
 });
